d417f57b6a3d17f19ca3597100155ec99393ccc3
Ivan Shumkov <ivan@shumkov.ru>
2018-02-06 15:50:52 +0530

    move RPC handlers to lib/api/rpc

--- bin/test.sh
+++ bin/test.sh
@@ -1,35 +1,57 @@
 #!/usr/bin/env bash
 
-# TODO: Obsolete
-
 curl -i -X POST -H "Content-Type: application/json; indent=4" \
 -d '{
 "jsonrpc": "2.0",
-"method": "getBlockchainUser",
-"params": {"name": "andy"},
+"method": "addTransitionPackets",
 "id": "1"
 }' http://localhost:5001;echo
 
 curl -i -X POST -H "Content-Type: application/json; indent=4" \
 -d '{
 "jsonrpc": "2.0",
-"method": "getBlockchainUserStateSinceHeight",
-"params": {"name": "andy", "height": "201234"},
+"method": "getDapSchemaByVersion",
 "id": "1"
 }' http://localhost:5001;echo
 
 curl -i -X POST -H "Content-Type: application/json; indent=4" \
 -d '{
 "jsonrpc": "2.0",
-"method": "getBlockchainUserState",
-"params": {"name": "andy", "dapid": "1234"},
+"method": "getDapSchemas",
 "id": "1"
 }' http://localhost:5001;echo
 
 curl -i -X POST -H "Content-Type: application/json; indent=4" \
 -d '{
 "jsonrpc": "2.0",
-"method": "getDapSchema",
-"params": {"dapid": "1234"},
+"method": "getDapSchemaVersions",
+"id": "1"
+}' http://localhost:5001;echo
+
+curl -i -X POST -H "Content-Type: application/json; indent=4" \
+-d '{
+"jsonrpc": "2.0",
+"method": "getTransitionPacket",
+"id": "1"
+}' http://localhost:5001;echo
+
+curl -i -X POST -H "Content-Type: application/json; indent=4" \
+-d '{
+"jsonrpc": "2.0",
+"method": "getUserObjectByRevision",
+"id": "1"
+}' http://localhost:5001;echo
+
+curl -i -X POST -H "Content-Type: application/json; indent=4" \
+-d '{
+"jsonrpc": "2.0",
+"method": "getUserObjectRevisions",
+"id": "1"
+}' http://localhost:5001;echo
+
+curl -i -X POST -H "Content-Type: application/json; indent=4" \
+-d '{
+"jsonrpc": "2.0",
+"method": "getUserObjects",
 "id": "1"
 }' http://localhost:5001;echo
--- scripts/rpc.js
+++ scripts/rpc.js
@@ -1,1 +1,12 @@
-// TODO: Implement internal RPC server
+const jayson = require('jayson/promise');
+const rpcHandlers = require('../lib/api/rpc');
+
+const server = jayson.server(rpcHandlers);
+
+const bindHost = process.env.BIND_HOST || '0.0.0.0';
+server.http().listen(5001, bindHost);
+
+// break on ^C
+process.on('SIGINT', () => {
+  process.exit();
+});
