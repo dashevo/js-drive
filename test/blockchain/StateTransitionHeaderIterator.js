06093f9d02a365fade418515652396f95ec6eee6
Ivan Shumkov <ivan@shumkov.ru>
2018-02-22 10:01:03 +0300

    add methods checks

--- test/blockchain/StateTransitionHeaderIterator.js
+++ test/blockchain/StateTransitionHeaderIterator.js
@@ -3,7 +3,11 @@
 const fs = require('fs');
 const path = require('path');
 
-const { expect } = require('chai');
+const { expect, use } = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+use(sinonChai);
 
 const StateTransitionHeaderIterator = require('../../lib/blockchain/StateTransitionHeaderIterator');
 const StateTransitionHeader = require('../../lib/blockchain/StateTransitionHeader');
@@ -36,6 +40,20 @@ const blockIteratorMock = {
 
 
 describe('StateTransitionHeaderIterator', () => {
+  let getTransitionHeaderSpy;
+  let nextSpy;
+
+  beforeEach(function beforeEach() {
+    if (!this.sinon) {
+      this.sinon = sinon.sandbox.create();
+    } else {
+      this.sinon.restore();
+    }
+
+    getTransitionHeaderSpy = this.sinon.spy(blockIteratorMock.rpcClient, 'getTransitionHeader');
+    nextSpy = this.sinon.spy(blockIteratorMock, 'next');
+  });
+
   it('should iterate over State Transitions from BlockIterator', async () => {
     const obtainedHeaders = [];
 
@@ -51,6 +69,9 @@ describe('StateTransitionHeaderIterator', () => {
 
       obtainedHeaders.push(header);
     }
+
+    expect(getTransitionHeaderSpy).has.callCount(transitionHeaders.length);
+    expect(nextSpy).has.callCount(blocks.length + 1);
 
     const transitionHeaderObjects = transitionHeaders.map(h => new StateTransitionHeader(h));
     expect(obtainedHeaders).to.be.deep.equal(transitionHeaderObjects);
