84af5f77e9bccaedc98250e5d5473cb009af5aea
Ivan Shumkov <ivan@shumkov.ru>
2018-02-26 02:01:42 +0300

    handle wrong block sequence error
    handle wrong block sequence error and read since stable block height

--- test/storage/addStateTransitionsFromBlockchain.js
+++ test/storage/addStateTransitionsFromBlockchain.js
@@ -10,13 +10,17 @@ use(sinonChai);
 const addStateTransitionsFromBlockchain = require('../../lib/storage/addStateTransitionsFromBlockchain');
 const StateTransitionHeader = require('../../lib/blockchain/StateTransitionHeader');
 const StateTransitionHeaderIterator = require('../../lib/blockchain/StateTransitionHeaderIterator');
+const WrongBlocksSequenceError = require('../../lib/blockchain/WrongBlocksSequenceError');
 
 
 describe('addStateTransitionsFromBlockchain', () => {
+  let blocks;
   let transitionHeaders;
   let ipfsAPIMock;
   let stateTransitionHeaderIteratorMock;
+  let currentHeaderIndex;
   let nextStab;
+  let setNextStubWithErrorOnSecondBlock;
 
   beforeEach(function beforeEach() {
     if (!this.sinon) {
@@ -24,6 +28,9 @@ describe('addStateTransitionsFromBlockchain', () => {
     } else {
       this.sinon.restore();
     }
+
+    const blocksJSON = fs.readFileSync(path.join(__dirname, '/../fixtures/blocks.json'));
+    blocks = JSON.parse(blocksJSON);
 
     const transitionHeadersJSON = fs.readFileSync(path.join(__dirname, '/../fixtures/stateTransitionHeaders.json'));
     const transitionHeadersData = JSON.parse(transitionHeadersJSON);
@@ -50,7 +57,7 @@ describe('addStateTransitionsFromBlockchain', () => {
     stateTransitionHeaderIteratorMock = new StateTransitionHeaderIterator(blockIteratorMock);
 
     nextStab = this.sinon.stub(stateTransitionHeaderIteratorMock, 'next');
-    let currentHeaderIndex = 0;
+    currentHeaderIndex = 0;
     nextStab.callsFake(() => {
       if (!transitionHeaders[currentHeaderIndex]) {
         return Promise.resolve({ done: true });
@@ -62,6 +69,32 @@ describe('addStateTransitionsFromBlockchain', () => {
 
       return Promise.resolve({ done: false, value: currentHeader });
     });
+
+    setNextStubWithErrorOnSecondBlock = function setNextStubWithError() {
+      let throwErrorOnSecondBlock = true;
+      nextStab.callsFake(() => {
+        if (!transitionHeaders[currentHeaderIndex]) {
+          return Promise.resolve({ done: true });
+        }
+
+        if (currentHeaderIndex === blocks[0].ts.length && throwErrorOnSecondBlock) {
+          throwErrorOnSecondBlock = false;
+
+          throw new WrongBlocksSequenceError();
+        }
+
+        const currentHeader = transitionHeaders[currentHeaderIndex];
+
+        currentHeaderIndex++;
+
+        return Promise.resolve({ done: false, value: currentHeader });
+      });
+    };
+
+    const resetStab = this.sinon.stub(stateTransitionHeaderIteratorMock, 'reset');
+    resetStab.callsFake(() => {
+      currentHeaderIndex = 0;
+    });
   });
 
   it('should pin ST packets by hash from ST headers from blockchain', async () => {
@@ -72,6 +105,32 @@ describe('addStateTransitionsFromBlockchain', () => {
     expect(ipfsAPIMock.pin.add).has.callCount(transitionHeaders.length);
 
     transitionHeaders.forEach((header) => {
+      expect(ipfsAPIMock.pin.add).to.be.calledWith(header.storageHash, { recursive: true });
+    });
+  });
+
+  it('should pin ST packets again since stable block if blocks sequence is wrong', async () => {
+    // Stub of "next" method should throws WrongBlocksSequenceError on second block
+    setNextStubWithErrorOnSecondBlock();
+
+    await addStateTransitionsFromBlockchain(ipfsAPIMock, stateTransitionHeaderIteratorMock);
+
+    // nextStab calls transitionHeaders.length + from from first block + error + last one empty
+    expect(nextStab).has.callCount(transitionHeaders.length + blocks[0].ts.length + 2);
+
+    // Pin add calls transitionHeaders.length + from first blocks
+    expect(ipfsAPIMock.pin.add).has.callCount(transitionHeaders.length + blocks[0].ts.length);
+
+    // Copy headers and duplicate headers from first block
+    const transitionHeadersWithDuplicate = transitionHeaders.slice();
+    // eslint-disable-next-line arrow-body-style
+    const tsHeadersFromFirstTwoBlocks = blocks[0].ts.map((tsid) => {
+      return transitionHeaders.find(h => h.tsid === tsid);
+    });
+
+    transitionHeadersWithDuplicate.unshift(...tsHeadersFromFirstTwoBlocks);
+
+    transitionHeadersWithDuplicate.forEach((header) => {
       expect(ipfsAPIMock.pin.add).to.be.calledWith(header.storageHash, { recursive: true });
     });
   });
