630f20bc1beb401d1e6fc7e70dc98930bd8b77c9
Ivan Shumkov <ivan@shumkov.ru>
2018-03-21 10:13:11 +0300

    move build back to bash script

--- bin/build_docker_images.sh
+++ bin/build_docker_images.sh
@@ -1,7 +1,22 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-# 0. authenticate your Docker client to your registry:
 eval $(aws ecr get-login --no-include-email)
 
-docker-compose build
-docker-compose push
+IMAGE_TAG_PREFIX="103738324493.dkr.ecr.us-west-2.amazonaws.com/dashevo/dashdrive"
+DOCKER_FILE_PATH="docker/node/Dockerfile"
+
+docker build --build-arg NODE_ENV=development \
+             --build-arg RUN_SCRIPT=api \
+             --build-arg EXPOSE_PORTS=80 9229 \
+             -f ${DOCKER_FILE_PATH} \
+             -t ${IMAGE_TAG_PREFIX}/api \
+             .
+
+docker build --build-arg NODE_ENV=development \
+             --build-arg RUN_SCRIPT=sync \
+             -f ${DOCKER_FILE_PATH} \
+             -t ${IMAGE_TAG_PREFIX}/sync \
+             .
+
+docker-compose push ${IMAGE_TAG_PREFIX}/api
+docker-compose push ${IMAGE_TAG_PREFIX}/sync
