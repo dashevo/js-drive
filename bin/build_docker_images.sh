a3e4051162d0795c9413f3e0e688fbc5e4d1100b
Ivan Shumkov <ivan@shumkov.ru>
2018-03-21 18:34:35 +0300

    fix images names

--- bin/build_docker_images.sh
+++ bin/build_docker_images.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# source .env
-# eval $(aws ecr get-login --no-include-email)
+set -e
 
 IMAGE_TAG_PREFIX="103738324493.dkr.ecr.us-west-2.amazonaws.com/dashevo/dashdrive"
 DOCKER_FILE_PATH="docker/node/Dockerfile"
@@ -10,14 +9,14 @@ docker build --build-arg NODE_ENV=development \
              --build-arg RUN_SCRIPT=api \
              --build-arg EXPOSE_PORTS=80 9229 \
              -f ${DOCKER_FILE_PATH} \
-             -t ${IMAGE_TAG_PREFIX}/api \
+             -t ${IMAGE_TAG_PREFIX}-api \
              .
 
 docker build --build-arg NODE_ENV=development \
              --build-arg RUN_SCRIPT=sync \
              -f ${DOCKER_FILE_PATH} \
-             -t ${IMAGE_TAG_PREFIX}/sync \
+             -t ${IMAGE_TAG_PREFIX}-sync \
              .
 
-docker-compose push ${IMAGE_TAG_PREFIX}/api
-docker-compose push ${IMAGE_TAG_PREFIX}/sync
+docker-compose push ${IMAGE_TAG_PREFIX}-api
+docker-compose push ${IMAGE_TAG_PREFIX}-sync
