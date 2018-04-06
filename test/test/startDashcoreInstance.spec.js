ad6b57e4827b5ce61bd95d1035b80219b8c4e9a4
David Rey <david.rey.acosta@gmail.com>
2018-04-06 17:38:31 +0200

    Add startDashcoreInstance for create multiple Dash core instances

---
+++ test/test/startDashcoreInstance.spec.js
@@ -0,0 +1,73 @@
+const { expect } = require('chai');
+const Docker = require('dockerode');
+
+const startDashcoreInstance = require('../../lib/test/startDashcoreInstance');
+
+async function stopRunningContainers() {
+  const docker = new Docker();
+  const containers = await docker.listContainers();
+
+  for (let i = 0; i < containers.length; i++) {
+    const container = containers[i];
+    await docker.getContainer(container.Id).stop();
+  }
+}
+
+describe('startDashcoreInstance', function main() {
+  this.timeout(20000);
+
+  before(async () => stopRunningContainers());
+
+  describe('One instance', () => {
+    let instance;
+
+    before(async () => {
+      instance = await startDashcoreInstance();
+    });
+    after(async () => {
+      await instance.clean();
+    });
+
+    it('should has container running', async () => {
+      const { State } = await instance.container.inspect();
+      expect(State.Status).to.equal('running');
+    });
+
+    it('should has RPC connected', async () => new Promise((resolve) => {
+      setTimeout(async () => {
+        const { result } = await instance.rpcClient.getinfo();
+        expect(result.version).to.equal(120300);
+        resolve();
+      }, 5000);
+    }));
+  });
+
+  describe('Three instances', () => {
+    let instances;
+
+    before(async () => {
+      instances = await startDashcoreInstance.many(3);
+    });
+    after(async () => {
+      const promises = instances.map(instance => instance.clean());
+      await Promise.all(promises);
+    });
+
+    it('should have containers running', async () => {
+      for (let i = 0; i < 3; i++) {
+        const { State } = await instances[i].container.inspect();
+        expect(State.Status).to.equal('running');
+      }
+    });
+
+    it('should have RPCs connected', async () => new Promise((resolve) => {
+      setTimeout(async () => {
+        for (let i = 0; i < 3; i++) {
+          const { result } = await instances[i].rpcClient.getinfo();
+          expect(result.version).to.equal(120300);
+        }
+        resolve();
+      }, 10000);
+    }));
+  });
+});
