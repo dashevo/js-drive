e589dab571cb5690d742bfca5dc496a79ee44b62
David Rey <david.rey.acosta@gmail.com>
2018-04-24 12:24:44 +0200

    startDashDriveInstance starts IPFS and DashCore

--- test/test/services/dashDrive/startDashDriveInstance.spec.js
+++ test/test/services/dashDrive/startDashDriveInstance.spec.js
@@ -1,13 +1,18 @@
 const startDashDriveInstance = require('../../../../lib/test/services/dashDrive/startDashDriveInstance');
 
 describe('startDashDriveInstance', function main() {
-  this.timeout(40000);
+  this.timeout(90000);
 
   describe('One instance', () => {
     let instance;
 
     before(async () => {
       instance = await startDashDriveInstance();
+    });
+
+    it('should has DashCore container running', async () => {
+      const { State } = await instance.dashCore.container.details();
+      expect(State.Status).to.equal('running');
     });
 
     it('should has MongoDb container running', async () => {
@@ -20,14 +25,39 @@ describe('startDashDriveInstance', function main() {
       expect(State.Status).to.equal('running');
     });
 
-    it('should has DashDrive container has the right MongoDb address', async () => {
+    it('should DashDrive container has the right MongoDb address', async () => {
       const { Config: { Env } } = await instance.dashDrive.container.details();
       const expectedEnv = `STORAGE_MONGODB_URL=mongodb://${instance.mongoDb.getIp()}`;
       const mongoAddressVariable = Env.filter(variable => variable === expectedEnv);
       expect(mongoAddressVariable.length).to.equal(1);
     });
 
-    it('should be on the same network (DashDrive and MongoDb)', async () => {
+    it('should DashDrive container has the right DashCore settings', async () => {
+      const { Config: { Env } } = await instance.dashDrive.container.details();
+      const expectedEnv = [
+        `DASHCORE_ZMQ_PUB_HASHBLOCK=${instance.dashCore.getZmqSockets().hashblock}`,
+        `DASHCORE_JSON_RPC_HOST=${instance.dashCore.getIp()}`,
+        `DASHCORE_JSON_RPC_PORT=${instance.dashCore.options.getMainPort()}`,
+        `DASHCORE_JSON_RPC_USER=${instance.dashCore.options.getRpcUser()}`,
+        `DASHCORE_JSON_RPC_PASS=${instance.dashCore.options.getRpcPassword()}`,
+      ];
+      const envs = Env.filter(variable => expectedEnv.indexOf(variable) !== -1);
+      expect(envs.length).to.equal(expectedEnv.length);
+    });
+
+    it('should DashDrive container has the right IPFS settings', async () => {
+      const { Config: { Env } } = await instance.dashDrive.container.details();
+      const expectedEnv = [
+        `STORAGE_IPFS_MULTIADDR=/ip4/${instance.ipfs.apiHost}/tcp/${instance.ipfs.apiPort}`,
+      ];
+      const envs = Env.filter(variable => expectedEnv.indexOf(variable) !== -1);
+      expect(envs.length).to.equal(expectedEnv.length);
+    });
+
+    it('should be on the same network (DashCore, DashDrive and MongoDb)', async () => {
+      const {
+        NetworkSettings: dashCoreNetworkSettings,
+      } = await instance.dashCore.container.details();
       const {
         NetworkSettings: dashDriveNetworkSettings,
       } = await instance.dashDrive.container.details();
@@ -35,6 +65,7 @@ describe('startDashDriveInstance', function main() {
         NetworkSettings: mongoDbNetworkSettings,
       } = await instance.mongoDb.container.details();
 
+      expect(Object.keys(dashCoreNetworkSettings.Networks)).to.deep.equal(['dash_test_network']);
       expect(Object.keys(dashDriveNetworkSettings.Networks)).to.deep.equal(['dash_test_network']);
       expect(Object.keys(mongoDbNetworkSettings.Networks)).to.deep.equal(['dash_test_network']);
     });
@@ -45,6 +76,13 @@ describe('startDashDriveInstance', function main() {
 
     before(async () => {
       instances = await startDashDriveInstance.many(3);
+    });
+
+    it('should have DashCore containers running', async () => {
+      for (let i = 0; i < 3; i++) {
+        const { State } = await instances[i].dashCore.container.details();
+        expect(State.Status).to.equal('running');
+      }
     });
 
     it('should have MongoDb containers running', async () => {
