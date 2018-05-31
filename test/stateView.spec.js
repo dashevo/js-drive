a11952f4da3a134645e913ab47d8a490ba6b055f
David Rey <david.rey.acosta@gmail.com>
2018-05-31 13:27:44 +0200

    Add State View unit tests

--- test/stateView.spec.js
+++ test/stateView.spec.js
@@ -22,7 +22,6 @@ class DapContract {
   getDapId() {
     return this.dapId;
   }
-
   getContract() {
     return this.contract;
   }
@@ -152,5 +151,70 @@ describe('State view implementation', () => {
       expect(dapContract.getDapId()).to.equal(dapId);
       expect(dapContract.getContract()).to.deep.equal(packet.schema);
     });
+  });
+});
+
+
+describe('State View', () => {
+  let dapContractRepository;
+  let ipfsClient;
+  beforeEach(function beforeEach() {
+    dapContractRepository = {
+      store: this.sinon.stub(),
+    };
+    ipfsClient = {
+      dag: {
+        get: this.sinon.stub(),
+      },
+    };
+  });
+
+  it('should throw an error if IPFS fails', async () => {
+    ipfsClient.dag.get.throws(new Error('IpfsError'));
+    const storeDapContract = storeDapContractFactory(dapContractRepository, ipfsClient);
+
+    try {
+      const cid = 'zdpuB1nHv2ewWb3k5dgm2FNuGsKohuujAg3uWJTopZsrxJiXG';
+      await storeDapContract(cid);
+    } catch (error) {
+      expect(error.message).to.equal('IpfsError');
+    }
+  });
+
+  xit('should throw an error if StateTransitionPacket fails', async () => {
+    ipfsClient.dag.get.returns({ value: { packet: 'shit' } });
+    const storeDapContract = storeDapContractFactory(dapContractRepository, ipfsClient);
+
+    try {
+      const cid = 'zdpuB1nHv2ewWb3k5dgm2FNuGsKohuujAg3uWJTopZsrxJiXG';
+      await storeDapContract(cid);
+    } catch (error) {
+      expect(error.message).to.equal('InvalidPacket');
+    }
+  });
+
+  it('should throw an error if DapContractRepository fails', async () => {
+    dapContractRepository.store.throws(new Error('RepositoryFails'));
+    const packet = getTransitionPacketFixtures()[0];
+    ipfsClient.dag.get.returns({ value: packet.toJSON() });
+    const storeDapContract = storeDapContractFactory(dapContractRepository, ipfsClient);
+
+    try {
+      const cid = 'zdpuB1nHv2ewWb3k5dgm2FNuGsKohuujAg3uWJTopZsrxJiXG';
+      await storeDapContract(cid);
+    } catch (error) {
+      expect(error.message).to.equal('RepositoryFails');
+    }
+  });
+
+  it('should return to work successfully', async () => {
+    const packet = getTransitionPacketFixtures()[0];
+    ipfsClient.dag.get.returns({ value: packet.toJSON() });
+    const storeDapContract = storeDapContractFactory(dapContractRepository, ipfsClient);
+
+    const cid = 'zdpuB1nHv2ewWb3k5dgm2FNuGsKohuujAg3uWJTopZsrxJiXG';
+    await storeDapContract(cid);
+
+    expect(dapContractRepository.store).to.be.calledOnce();
   });
 });
