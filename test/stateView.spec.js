e4c6671c1bf8be9a6d1fdb1120b7daa8fb488a2e
David Rey <david.rey.acosta@gmail.com>
2018-06-01 17:48:39 +0200

    Add storeDapContractHandler

--- test/stateView.spec.js
+++ test/stateView.spec.js
@@ -269,3 +269,34 @@ describe('State View', () => {
     expect(dapContractRepository.store).to.be.calledOnce();
   });
 });
+
+/**
+ * Store DAP contract handler
+ *
+ * @param {STHeadersReader} stHeadersReader
+ * @param {storeDapContract} storeDapContract
+ */
+function storeDapContractHandler(stHeadersReader, storeDapContract) {
+  stHeadersReader.on('header', async (header) => {
+    const cid = header.getPacketCID();
+    await storeDapContract(cid);
+  });
+}
+
+describe('storeDapContractHandler', () => {
+  let stHeadersReader;
+  let storeDapContract;
+
+  beforeEach(function beforeEach() {
+    const header = getTransitionHeaderFixtures()[0];
+    stHeadersReader = {
+      on: (topic, fn) => fn(header),
+    };
+    storeDapContract = this.sinon.stub();
+    storeDapContractHandler(stHeadersReader, storeDapContract);
+  });
+
+  it('should call storeDapContractHandler on new block header', () => {
+    expect(storeDapContract).to.be.calledOnce();
+  });
+});
