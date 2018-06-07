f7601f42269842311d18d48a0bfb3f9dffcc4144
Djavid Gabibiyan <djavidoff@gmail.com>
2018-06-07 12:09:26 +0500

    added unit tests for wait utils module

---
+++ test/unit/utils/wait.js
@@ -0,0 +1,17 @@
+describe('wait', () => {
+  it('should be globally defined for tests', () => {
+    expect(wait).to.exist();
+  });
+
+  it('should delay execution of a flow for a specified amount of milliseconds', async () => {
+    const millisToWait = 1200;
+
+    const startTime = process.hrtime();
+    await wait(millisToWait);
+    const endTime = process.hrtime(startTime);
+
+    const endTimeMillis = (endTime[0] * 1e3) + (endTime[1] / 1e6);
+
+    expect(endTimeMillis).to.be.at.least(millisToWait);
+  });
+});
