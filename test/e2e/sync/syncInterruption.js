674911c59bbf00cc687152d8bf3fd8a4879e36fa
Djavid Gabibiyan <djavidoff@gmail.com>
2018-06-18 20:26:46 +0500

    updated test code with fixes from DD-98

--- test/e2e/sync/syncInterruption.js
+++ test/e2e/sync/syncInterruption.js
@@ -1,20 +1,15 @@
 const addSTPacketFactory = require('../../../lib/storage/addSTPacketFactory');
-
-const StateTransitionPacket = require('../../../lib/storage/StateTransitionPacket');
-
 const getStateTransitionPackets = require('../../../lib/test/fixtures/getTransitionPacketFixtures');
+const generateStateTransitions = require('../../../lib/test/fixtures/generateStateTransitions');
 
 const startDashDriveInstance = require('../../../lib/test/services/dashDrive/startDashDriveInstance');
+const startDashCoreInstance = require('../../../lib/test/services/dashCore/startDashCoreInstance');
+const startMongoDbInstance = require('../../../lib/test/services/mongoDb/startMongoDbInstance');
 const startIPFSInstance = require('../../../lib/test/services/IPFS/startIPFSInstance');
 
 const createDashDriveInstance = require('../../../lib/test/services/dashDrive/createDashDriveInstance');
-const createDashCoreInstance = require('../../../lib/test/services/dashCore/createDashCoreInstance');
-const createMongoDbInstance = require('../../../lib/test/services/mongoDb/createMongoDbInstance');
 
-const gst = require('../../../lib/test/fixtures/generateStateTransitions');
-
-const jayson = require('jayson');
-
+const wait = require('../../../lib/test/util/wait');
 const cbor = require('cbor');
 
 describe('Sync interruption and resume between Dash Drive and Dash Core', function main() {
@@ -25,156 +20,124 @@ describe('Sync interruption and resume between Dash Drive and Dash Core', functi
   let dashCoreInstance;
   let mongoDbInstance;
   let dashDriveInstance2;
+  let ipfsInstance;
 
-  let packetsCids = [];
-
+  let packetsCids;
   let packetsData;
 
-  this.timeout(300000);
+  this.timeout(900000);
 
   before('having Dash Drive node #1 up and running', async () => {
     dashDriveInstance = await startDashDriveInstance();
 
+    packetsCids = [];
     packetsData = getStateTransitionPackets();
 
     async function createAndSubmitST(username) {
-      const { userId, privateKeyString } = await gst.registerUser(
-	username, dashDriveInstance.dashCore.rpcClient
-      );
+      const packetOne = packetsData[0];
+      packetOne.data.objects[0].description = `Valid registration for ${username}`;
 
-      const [packet, header] = await gst.createDapContractTransitions(
-	userId, privateKeyString, packetsData[0]
-      );
+      const { userId, privateKeyString } = await generateStateTransitions
+        .registerUser(username, dashDriveInstance.dashCore.rpcClient);
+      const [packet, header] = await generateStateTransitions
+        .createDapContractTransitions(userId, privateKeyString, packetOne);
 
-      const addSTPacket = addSTPacketFactory(dashDriveInstance.ipfs);
+      const addSTPacket = addSTPacketFactory(dashDriveInstance.ipfs.getApi());
       const packetCid = await addSTPacket(packet);
 
       packetsCids.push(packetCid);
 
-      const txid = await dashDriveInstance.dashCore.rpcClient.sendRawTransition(header);
-
-      return txid;
+      await dashDriveInstance.dashCore.rpcClient.sendRawTransition(header);
+      await dashDriveInstance.dashCore.rpcClient.generate(1);
     }
 
-    for(let i = 0; i < 50; i++) {
-      const txid = await createAndSubmitST(`Alice_${i}`);
+    for (let i = 0; i < 50; i++) {
+      await createAndSubmitST(`Alice_${i}`);
     }
-
-    const txids = await dashDriveInstance.dashCore.rpcClient.generate(60);
   });
 
   it('Dash Drive should save sync state and continue from saved point after resume', async () => {
-    dashCoreInstance = await createDashCoreInstance();
-    await dashCoreInstance.start();
-
+    dashCoreInstance = await startDashCoreInstance();
     await dashCoreInstance.connect(dashDriveInstance.dashCore);
 
-    mongoDbInstance = await createMongoDbInstance();
-    await mongoDbInstance.start();
+    mongoDbInstance = await startMongoDbInstance();
 
-    const ipfsInstance = await startIPFSInstance();
-    const { apiHost, apiPort } = ipfsInstance;
+    ipfsInstance = await startIPFSInstance();
+    await ipfsInstance.connect(dashDriveInstance.ipfs);
 
-    const firstInstanceId = await dashDriveInstance.ipfs.id();
-
-    await ipfsInstance.swarm.connect(
-      firstInstanceId.addresses[0]
-    );
-
-    const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-    while(true) {
-      const status = await dashCoreInstance.rpcClient.mnsync('status');
-      if (status.result.IsSynced === true) {
-	break;
+    async function dashCoreSyncToFinish(instance) {
+      let finished = false;
+      while (!finished) {
+        const status = await instance.rpcClient.mnsync('status');
+        if (status.result.IsSynced) {
+          finished = true;
+        } else {
+          await wait(3000);
+        }
       }
-      await wait(3000);
     }
+
+    await dashCoreSyncToFinish(dashCoreInstance);
 
     // start Dash Drive on node #2
     const envs = [
-      `DASHCORE_ZMQ_PUB_HASHBLOCK=${dashCoreInstance.options.getZmqSockets().hashblock}`,
+      `DASHCORE_ZMQ_PUB_HASHBLOCK=${dashCoreInstance.getZmqSockets().hashblock}`,
       `DASHCORE_JSON_RPC_HOST=${dashCoreInstance.getIp()}`,
       `DASHCORE_JSON_RPC_PORT=${dashCoreInstance.options.getRpcPort()}`,
       `DASHCORE_JSON_RPC_USER=${dashCoreInstance.options.getRpcUser()}`,
       `DASHCORE_JSON_RPC_PASS=${dashCoreInstance.options.getRpcPassword()}`,
-      `STORAGE_IPFS_MULTIADDR=/ip4/${apiHost}/tcp/${apiPort}`,
+      `STORAGE_IPFS_MULTIADDR=${ipfsInstance.getIpfsAddress()}`,
       `STORAGE_MONGODB_URL=mongodb://${mongoDbInstance.getIp()}`,
     ];
 
-    let lsResult = await ipfsInstance.pin.ls();
+    let lsResult = await ipfsInstance.getApi().pin.ls();
     const initialHashes = lsResult.map(item => item.hash);
 
     dashDriveInstance2 = await createDashDriveInstance(envs);
     await dashDriveInstance2.start();
 
-    const ddPort = await dashDriveInstance2.getRpcPort();
-
-    // Shamelesly taken from David
-    async function createRpcClient(rpcPort) {
-      const client = jayson.client.http({
-	host: '0.0.0.0',
-	port: rpcPort,
-      });
-
-      return new Promise((resolve) => {
-	function request() {
-          client.request('', [], (error) => {
-            if (error && error.message === 'socket hang up') {
-              return setTimeout(request, 1000);
-            }
-            return resolve(client);
-          });
-	}
-	request();
-      });
-    }
-
-    const driveRpc = await createRpcClient(ddPort);
-
-    const simplePacket = {
-      packet: cbor.encodeCanonical(packetsData[0])
+    const serializedPacket = cbor.encodeCanonical(packetsData[0]);
+    const spJson = {
+      packet: serializedPacket.toString('hex'),
     };
 
-    async function dashDriveSyncToFinish() {
+    async function dashDriveSyncToFinish(instance) {
       let finished = false;
-      while(!finished) {
-	driveRpc.request('addSTPacketMethod', simplePacket, (err, res) => {
-	  if (err) {
-	    return;
-	  }
-	  if (res.error && res.error.code === 100) {
-	    return;
-	  }
-	  finished = true;
-	});
-	await wait(1000);
+      while (!finished) {
+        try {
+          const response = await instance.getApi().request('addSTPacketMethod', spJson);
+          if (response.result) {
+            finished = true;
+          } else {
+            await wait(1000);
+          }
+        } catch (e) {
+          await wait(1000);
+        }
       }
     }
 
-    // TODO: figure out a proper timeout once error with RPC resolved.
-    // Or count pinned hashes and continue once there are 10 of them, for example.
+    // Wait a couple of seconds to sync a few packets
     for (let i = 0; i < 5; i++) {
       await wait(1000);
     }
 
     await dashDriveInstance2.stop();
 
-    lsResult = await ipfsInstance.pin.ls();
+    lsResult = await ipfsInstance.getApi().pin.ls();
     const pinnedHashes = lsResult
-	  .filter(item => initialHashes.indexOf(item.hash) === -1)
-	  .map(item => item.hash);
+      .filter(item => initialHashes.indexOf(item.hash) === -1)
+      .map(item => item.hash);
 
-    const rmPromises = Promise.all(
-      pinnedHashes.map(hash => ipfsInstance.pin.rm(hash))
-    );
+    const rmPromises = Promise
+      .all(pinnedHashes.map(hash => ipfsInstance.getApi().pin.rm(hash)));
     await rmPromises;
 
     await dashDriveInstance2.start();
 
-    await dashDriveSyncToFinish();
+    await dashDriveSyncToFinish(dashDriveInstance2);
 
-    lsResult = await ipfsInstance.pin.ls();
+    lsResult = await ipfsInstance.getApi().pin.ls();
 
     const hashesAfterResume = lsResult.map(item => item.hash);
 
@@ -187,6 +150,7 @@ describe('Sync interruption and resume between Dash Drive and Dash Core', functi
       dashCoreInstance.remove(),
       dashDriveInstance.remove(),
       dashDriveInstance2.remove(),
+      ipfsInstance.remove(),
     ]);
     await promises;
   });
