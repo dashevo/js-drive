187fa2f080f1e8af0a4599f1167e9bb2c4edd160
Ivan Shumkov <ivan@shumkov.ru>
2018-06-24 20:33:20 +0300

    Use valid CID

--- test/integration/storage/pinUnknown.js
+++ test/integration/storage/pinUnknown.js
@@ -1,14 +1,26 @@
 const startIPFSInstance = require('../../../lib/test/services/mocha/startIPFSInstance');
 
+const StateTransitionHeader = require('../../../lib/blockchain/StateTransitionHeader');
+
+const getTransitionHeaderFixtures = require('../../../lib/test/fixtures/getTransitionHeaderFixtures');
+
 describe('pinUnknown', () => {
   let ipfsApi;
+  let stHeader;
 
   startIPFSInstance.many(2).then(([instance]) => {
     ipfsApi = instance.getApi();
   });
 
-  it('pin unknown package', (done) => {
-    const closure = () => ipfsApi.pin.add('unknown');
+  before(() => {
+    const [stHeaderData] = getTransitionHeaderFixtures();
+    stHeader = new StateTransitionHeader(stHeaderData);
+  });
+
+  it('pin unknown package', function it(done) {
+    this.timeout(60000);
+
+    const closure = () => ipfsApi.pin.add(stHeader.getPacketCID());
     const error = "invalid 'ipfs ref' path";
 
     expect(closure()).to.eventually.be.rejectedWith(error).and.notify(done);
