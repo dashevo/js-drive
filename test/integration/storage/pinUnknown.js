54d26047718733af31a8a8cf3eb19aae67bac386
Ivan Shumkov <ivan@shumkov.ru>
2018-06-24 23:12:52 +0300

    Update tests

--- test/integration/storage/pinUnknown.js
+++ test/integration/storage/pinUnknown.js
@@ -2,27 +2,74 @@ const startIPFSInstance = require('../../../lib/test/services/mocha/startIPFSIns
 
 const StateTransitionHeader = require('../../../lib/blockchain/StateTransitionHeader');
 
+const throwAfter = require('../../../lib/util/throwAfter');
+
+const wait = require('../../../lib/test/util/wait');
+
 const getTransitionHeaderFixtures = require('../../../lib/test/fixtures/getTransitionHeaderFixtures');
 
-describe('pinUnknown', () => {
+describe('pinUnknown', function pinUnknown() {
   let ipfsApi;
   let stHeader;
 
-  startIPFSInstance.many(2).then(([instance]) => {
+  this.timeout(10000);
+
+  startIPFSInstance().then((instance) => {
     ipfsApi = instance.getApi();
   });
 
-  before(() => {
+  beforeEach(() => {
     const [stHeaderData] = getTransitionHeaderFixtures();
     stHeader = new StateTransitionHeader(stHeaderData);
   });
 
-  it('pin unknown package', function it(done) {
-    this.timeout(60000);
+  it('pin gets stuck', async () => {
+    await ipfsApi.pin.add(stHeader.getPacketCID());
+  });
 
+  it('pin gets stuck with unknown CID and throws context canceled error on Node.JS process exit', async () => {
     const closure = () => ipfsApi.pin.add(stHeader.getPacketCID());
-    const error = "invalid 'ipfs ref' path";
+    const promise = closure();
 
-    expect(closure()).to.eventually.be.rejectedWith(error).and.notify(done);
+    await wait(5000);
+
+    expect(promise).to.be.rejected('context canceled');
+
+    // expect(promise).to.not.be.rejected() leads to
+    // Error: pin: failed to get block for ...: context canceled
+    // from https://github.com/ipfs/go-ipfs/blob/08fb11fa896704d9e081bf64e66fac1f6d9d03dc/merkledag/merkledag.go#L77
+  });
+
+  it('pin throws error after timeout', (done) => {
+    const error = new Error('timeout');
+
+    const closure = () => {
+      const promise = ipfsApi.pin.add(stHeader.getPacketCID());
+
+      return throwAfter(promise, error, 5000);
+    };
+
+    expect(closure()).to.be.eventually.rejectedWith(error).and.notify(done);
+  });
+
+  it('pin throws error after timeout and remove cid from wantlist', async () => {
+    const error = new Error('timeout');
+    const cid = stHeader.getPacketCID();
+    const promise = ipfsApi.pin.add(cid);
+
+    try {
+      await throwAfter(promise, error, 5000);
+    } catch (e) {
+      if (e !== error) {
+        throw error;
+      }
+      await ipfsApi.bitswap.unwant(cid);
+    }
+
+    const { Keys: keys } = await ipfsApi.bitswap.wantlist();
+
+    expect(keys).to.be.empty();
+
+    // TODO: It seems we need to cancel request somehow before removing from this CID from wantlist
   });
 });
