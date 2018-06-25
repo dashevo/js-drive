6ea6ce33710759c587a92b607a285caea98f0761
Djavid Gabibiyan <djavidoff@gmail.com>
2018-06-25 22:50:41 +0500

    update test code for a single DD node

--- test/e2e/blockchainReorganization.js
+++ test/e2e/blockchainReorganization.js
@@ -10,25 +10,6 @@ const wait = require('../../lib/test/util/wait');
 const cbor = require('cbor');
 
 /**
- * Await Dash Core instance to finish syncing
- *
- * @param {DashCoreInstance} instance
- * @returns {Promise<void>}
- */
-async function dashCoreSyncToFinish(instance) {
-  let finished = false;
-  while (!finished) {
-    const status = await instance.rpcClient.mnsync('status');
-    console.log(status);
-    if (status.result.IsSynced) {
-      finished = true;
-    } else {
-      await wait(3000);
-    }
-  }
-}
-
-/**
  * Await Dash Drive instance to finish syncing
  *
  * @param {DashDriveInstance} instance
@@ -58,10 +39,10 @@ async function dashDriveSyncToFinish(instance) {
 }
 
 describe('Blockchain reorganization', function main() {
-  let firstDashDriveInstance;
-  let secondDashDriveInstance;
+  let dashDriveInstance;
 
   let packetsCids;
+  let lastPacketCid;
 
   let createAndSubmitST;
 
@@ -71,10 +52,10 @@ describe('Blockchain reorganization', function main() {
     packetsCids = [];
     const packetsData = getStateTransitionPackets();
 
-    // 1. Start both full Dash Drive instances
-    [firstDashDriveInstance, secondDashDriveInstance] = await startDashDriveInstance.many(2);
+    // 1. Start one full Dash Drive instances
+    dashDriveInstance = await startDashDriveInstance();
 
-    // 2. Populate first instance of Dash Drive and Dash Core with data
+    // 2. Populate instance of Dash Drive and Dash Core with data
     createAndSubmitST = async (username) => {
       // 2.1 Get packet data with random object description
       const packetOne = packetsData[0];
@@ -82,65 +63,53 @@ describe('Blockchain reorganization', function main() {
 
       // 2.2 Register user and create DAP Contract State Transition packet and header
       const { userId, privateKeyString } =
-        await registerUser(username, firstDashDriveInstance.dashCore.rpcClient);
+        await registerUser(username, dashDriveInstance.dashCore.rpcClient);
       const [packet, header] = await createDapContractST(userId, privateKeyString, packetOne);
 
       // 2.3 Add ST packet to IPFS
-      const addSTPacket = addSTPacketFactory(firstDashDriveInstance.ipfs.getApi());
+      const addSTPacket = addSTPacketFactory(dashDriveInstance.ipfs.getApi());
       const packetCid = await addSTPacket(packet);
 
       // 2.4 Save CID of freshly added packet for future use
       packetsCids.push(packetCid);
 
       // 2.5 Send ST header to Dash Core and generate a block with it
-      await firstDashDriveInstance.dashCore.rpcClient.sendRawTransition(header);
-      await firstDashDriveInstance.dashCore.rpcClient.generate(1);
+      await dashDriveInstance.dashCore.rpcClient.sendRawTransition(header);
+      await dashDriveInstance.dashCore.rpcClient.generate(1);
     };
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 4; i++) {
       await createAndSubmitST(`Alice_${i}`);
     }
 
-    // 3. Await second node of Dash Core and Dash Drive to sync
-    await dashCoreSyncToFinish(secondDashDriveInstance.dashCore);
-    await dashDriveSyncToFinish(secondDashDriveInstance);
+    lastPacketCid = packetsCids[packetsCids.length - 1];
   });
 
-  it('Dash Drive should sync data after blockchain reorganization, removing uncessary data.' +
-     'Dash Drive on another node should replicate data from the first one.', async () => {
-    // 4. Invalidate half of the generated ST blocks
-    await firstDashDriveInstance.dashCore.rpcClient.invalidate(10);
+  it('Dash Drive should sync data after blockchain reorganization, removing uncessary data. Dash Drive on another node should replicate data from the first one.', async () => {
+    // 4. Invalidate block that is 6 blocks away from the top
+    const { result: blockCount } = await dashDriveInstance.dashCore.rpcClient.getBlockCount();
+    const { result: blockHashToInvalidate } = await dashDriveInstance.dashCore.rpcClient
+      .getBlockHash(blockCount - 6);
 
-    // 5. Generate one new ST block
+    await dashDriveInstance.dashCore.rpcClient.invalidateBlock(blockHashToInvalidate);
+
+    // 5. Generate two new STs
     await createAndSubmitST('Bob');
+    await createAndSubmitST('John');
 
-    // 6. Awat Dash Core on 2nd node to sync
-    await dashCoreSyncToFinish(secondDashDriveInstance.dashCore);
+    // 6. Await Dash Drive to sync latest changes
+    await dashDriveSyncToFinish(dashDriveInstance.dashDrive);
 
-    // 7. Await both Dash Drive nodes to sync latest changes
-    await dashDriveSyncToFinish(firstDashDriveInstance);
-    await dashDriveSyncToFinish(secondDashDriveInstance);
+    // 7. Check that lastly submitted by createAndSubmitST method ST is gone from IPFS
+    const lsResult = await dashDriveInstance.ipfs.getApi().pin.ls();
+    const currentCids = lsResult.map(item => item.hash);
 
-    // 8. Check last half of packets have been removed
-    //    As half of the blocks (10) have been removed as well
-    const lastHalfHashes = packetsCids.splice(10, 20);
-
-    let lsResult = await firstDashDriveInstance.ipfs.getApi().pin.ls();
-    const firstNodeHashes = lsResult.map(item => item.hash);
-
-    lsResult = await secondDashDriveInstance.ipfs.getApi().pin.ls();
-    const secondNodeHashes = lsResult.map(item => item.hash);
-
-    expect(firstNodeHashes).to.not.contain.members(lastHalfHashes);
-    expect(secondNodeHashes).to.not.contain.members(lastHalfHashes);
-
-    expect(firstNodeHashes).to.have.members(secondNodeHashes);
+    expect(currentCids).to.not.include(lastPacketCid);
   });
 
   after('cleanup lone services', async () => {
     const promises = Promise.all([
-      firstDashDriveInstance.remove(),
-      secondDashDriveInstance.remove(),
+      dashDriveInstance.remove(),
     ]);
     await promises;
   });
