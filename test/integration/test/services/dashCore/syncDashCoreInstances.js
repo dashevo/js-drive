87f0b74b6ca2deea0c47f6c1eb5eba5e6132c466
Djavid Gabibiyan <djavidoff@gmail.com>
2018-07-02 17:42:05 +0500

    initial approach to syncDashCoreInstances test

---
+++ test/integration/test/services/dashCore/syncDashCoreInstances.js
@@ -0,0 +1,149 @@
+const removeContainers = require('../../../../../lib/test/services/docker/removeContainers');
+const startDashCoreInstance = require('../../../../../lib/test/services/dashCore/startDashCoreInstance');
+
+const getStateTransitionPackets = require('../../../../../lib/test/fixtures/getTransitionPacketFixtures');
+
+const registerUser = require('../../../../../lib/test/registerUser');
+const createDapContractST = require('../../../../../lib/test/createDapContractST');
+
+const wait = require('../../../../../lib/test/util/wait');
+
+/**
+ * Await Dash Core instance to finish syncing
+ *
+ * @param {DashCoreInstance} instance
+ * @returns {Promise<void>}
+ */
+async function dashCoreSyncToFinish(instance, name, timeout = 90000) {
+  let finished = false;
+  const startedAt = process.hrtime();
+  while (!finished) {
+    const status = await instance.rpcClient.mnsync('status');
+    const now = process.hrtime(startedAt);
+    const millisPassed = ((now[0] * 1e9) + now[1]) / 1e6;
+    if (status.result.IsSynced) {
+      finished = true;
+    } else if (millisPassed >= timeout) {
+      throw new Error(`Timeout waiting for Dash Core (${name} node) to sync`);
+    } else {
+      await wait(3000);
+    }
+  }
+}
+
+async function createAndSubmitST(username, packetData, dcInstance) {
+  const packetOne = packetData;
+  packetOne.data.objects[0].description = `Valid registration for ${username}`;
+
+  const { userId, privateKeyString } =
+        await registerUser(username, dcInstance.rpcClient);
+  const [, header] = await createDapContractST(userId, privateKeyString, packetOne);
+
+  await dcInstance.rpcClient.sendRawTransition(header);
+  await dcInstance.rpcClient.generate(1);
+}
+
+describe('syncDashCoreInstances', function main() {
+  this.timeout(900000);
+
+  describe('Dash Core Instances', () => {
+    beforeEach(removeContainers);
+
+    it('should have different number of blocks even if `msync("next")` called a few times', async () => {
+      const [firstInstance, secondInstance] = await startDashCoreInstance.many(2);
+
+      for (let i = 0; i < 6; i++) {
+        await firstInstance.rpcClient.mnsync('next');
+        await secondInstance.rpcClient.mnsync('next');
+      }
+
+      const packetsData = getStateTransitionPackets();
+
+      for (let i = 0; i < 20; i++) {
+        await createAndSubmitST(`Alice_${i}`, packetsData[0], firstInstance);
+      }
+
+      await dashCoreSyncToFinish(firstInstance, 'first');
+      await dashCoreSyncToFinish(secondInstance, 'second');
+
+      for (let i = 0; i < 90; i++) {
+        const { result: firstBlockCount } = await firstInstance.rpcClient.getBlockCount();
+        const { result: secondBlockCount } = await secondInstance.rpcClient.getBlockCount();
+
+        if (firstBlockCount === secondBlockCount) {
+          break;
+        }
+
+        await wait(1000);
+      }
+
+      const { result: firstBlockCount } = await firstInstance.rpcClient.getBlockCount();
+      const { result: secondBlockCount } = await secondInstance.rpcClient.getBlockCount();
+
+      expect(firstBlockCount).to.be.not.equal(secondBlockCount);
+    });
+
+    it('should eventually have the same number of blocks if both instances connected', async () => {
+      const [firstInstance, secondInstance] = await startDashCoreInstance.many(2);
+      secondInstance.connect(firstInstance);
+
+      const packetsData = getStateTransitionPackets();
+
+      for (let i = 0; i < 20; i++) {
+        await createAndSubmitST(`Alice_${i}`, packetsData[0], firstInstance);
+      }
+
+      await dashCoreSyncToFinish(firstInstance, 'first');
+      await dashCoreSyncToFinish(secondInstance, 'second');
+
+      for (let i = 0; i < 90; i++) {
+        const { result: firstBlockCount } = await firstInstance.rpcClient.getBlockCount();
+        const { result: secondBlockCount } = await secondInstance.rpcClient.getBlockCount();
+
+        if (firstBlockCount === secondBlockCount) {
+          break;
+        }
+
+        await wait(1000);
+      }
+
+      const { result: firstBlockCount } = await firstInstance.rpcClient.getBlockCount();
+      const { result: secondBlockCount } = await secondInstance.rpcClient.getBlockCount();
+
+      expect(firstBlockCount).to.be.equal(secondBlockCount);
+    });
+
+    it('should eventually have the same number of blocks if second instances connected to the first', async () => {
+      const firstInstance = await startDashCoreInstance();
+      const secondInstance = await startDashCoreInstance();
+      secondInstance.connect(firstInstance);
+
+      const packetsData = getStateTransitionPackets();
+
+      for (let i = 0; i < 20; i++) {
+        await createAndSubmitST(`Alice_${i}`, packetsData[0], firstInstance);
+      }
+
+      await dashCoreSyncToFinish(firstInstance, 'first');
+      await dashCoreSyncToFinish(secondInstance, 'second');
+
+      for (let i = 0; i < 90; i++) {
+        const { result: firstBlockCount } = await firstInstance.rpcClient.getBlockCount();
+        const { result: secondBlockCount } = await secondInstance.rpcClient.getBlockCount();
+
+        if (firstBlockCount === secondBlockCount) {
+          break;
+        }
+
+        await wait(1000);
+      }
+
+      const { result: firstBlockCount } = await firstInstance.rpcClient.getBlockCount();
+      const { result: secondBlockCount } = await secondInstance.rpcClient.getBlockCount();
+
+      expect(firstBlockCount).to.be.equal(secondBlockCount);
+    });
+
+    after(removeContainers);
+  });
+});
