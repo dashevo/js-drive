d965b4476356483a2901e7a99d6bf23961647933
David Rey <david.rey.acosta@gmail.com>
2018-07-13 12:38:20 +0700

    Add isNew method to DapObject

--- test/unit/stateView/dapObject/DapObject.js
+++ test/unit/stateView/dapObject/DapObject.js
@@ -1,13 +1,63 @@
+const Reference = require('../../../../lib/stateView/Reference');
 const DapObject = require('../../../../lib/stateView/dapObject/DapObject');
 
 describe('DapObject', () => {
   it('should serialize DapObject', () => {
-    const id = '123456';
-    const dapObject = new DapObject(id);
+    const blockHash = 'b8ae412cdeeb4bb39ec496dec34495ecccaf74f9fa9eaa712c77a03eb1994e75';
+    const blockHeight = 1;
+    const headerHash = '17jasdjk129uasd8asd023098SD09023jll123jlasd90823jklD';
+    const hashSTPacket = 'ad877138as8012309asdkl123l123lka908013';
+    const objectHash = '123981as90d01309ad09123';
+    const reference = new Reference(
+      blockHash,
+      blockHeight,
+      headerHash,
+      hashSTPacket,
+      objectHash,
+    );
+    const dapObjectData = {
+      id: '1234',
+      objtype: 'user',
+      idx: 0,
+      rev: 1,
+      act: 1,
+    };
+    const dapObject = new DapObject(reference, dapObjectData);
 
     const dapObjectSerialized = dapObject.toJSON();
     expect(dapObjectSerialized).to.deep.equal({
-      id,
+      id: dapObjectData.id,
+      type: dapObjectData.objtype,
+      object: dapObjectData,
+      revision: dapObjectData.rev,
+      reference: reference.toJSON(),
     });
+  });
+
+  it('should be new if DapObject action is 0', async () => {
+    const reference = new Reference();
+    const dapObjectData = {
+      id: '1234',
+      objtype: 'user',
+      idx: 0,
+      rev: 1,
+      act: 0,
+    };
+    const dapObject = new DapObject(reference, dapObjectData);
+    expect(dapObject.isNew()).to.be.true();
+  });
+
+  it('should not be new if DapObject action is not 0', async () => {
+    const reference = new Reference();
+    const dapObjectData = {
+      id: '1234',
+      objtype: 'user',
+      idx: 0,
+      rev: 1,
+      act: 0,
+
+    };
+    const dapObject = new DapObject(reference, dapObjectData);
+    expect(dapObject.isNew()).to.be.false();
   });
 });
