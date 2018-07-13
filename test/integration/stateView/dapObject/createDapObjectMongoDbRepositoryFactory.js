d1c5949f3e1c25466db48df65b2418a95a4443bd
David Rey <david.rey.acosta@gmail.com>
2018-07-13 12:40:33 +0700

    Update DapObjectMongoDbRepository with Reference

--- test/integration/stateView/dapObject/createDapObjectMongoDbRepositoryFactory.js
+++ test/integration/stateView/dapObject/createDapObjectMongoDbRepositoryFactory.js
@@ -1,4 +1,5 @@
 const DapObject = require('../../../../lib/stateView/dapObject/DapObject');
+const Reference = require('../../../../lib/stateView/Reference');
 const createDapObjectMongoDbRepositoryFactory = require('../../../../lib/stateView/dapObject/createDapObjectMongoDbRepositoryFactory');
 const DapObjectMongoDbRepository = require('../../../../lib/stateView/dapObject/DapObjectMongoDbRepository');
 const startMongoDbInstance = require('../../../../lib/test/services/mocha/startMongoDbInstance');
@@ -18,7 +19,11 @@ describe('createDapObjectMongoDbRepositoryFactory', () => {
     const dapObjectRepository = createDapObjectMongoDbRepository(dapId);
 
     const dapObjectId = '98765';
-    const dapObject = new DapObject(dapObjectId);
+    const reference = new Reference();
+    const objectData = {
+      id: dapObjectId,
+    };
+    const dapObject = new DapObject(reference, objectData);
     await dapObjectRepository.store(dapObject);
 
     const result = await dapObjectRepository.find(dapObjectId);
