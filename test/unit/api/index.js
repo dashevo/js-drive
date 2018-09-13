eeb8ab3aaeea462b314f10a87b03ad4b2f0e3c9a
David Rey <david.rey.acosta@gmail.com>
2018-09-14 00:07:07 +0200

    Add getSyncStatusMethod

--- test/unit/api/index.js
+++ test/unit/api/index.js
@@ -1,7 +1,4 @@
 /* eslint-disable global-require */
 describe('Api', () => {
-  describe('Methods', () => {
-    require('./methods/addSTPacketMethod');
-    require('./methods/fetchDapContractMethod');
-  });
+  require('./methods');
 });
---
+++ test/unit/api/methods/getSyncStatusMethodFactory.js
@@ -0,0 +1,45 @@
+const getBlockFixtures = require('../../../../lib/test/fixtures/getBlockFixtures');
+const SyncStatus = require('../../../../lib/sync/SyncStatus');
+const getSyncStatusMethodFactory = require('../../../../lib/api/methods/getSyncStatusMethodFactory');
+
+describe('getSyncStatusMethodFactory', () => {
+  let getSyncStatus;
+  let getSyncStatusMethod;
+  const blocks = getBlockFixtures();
+
+  beforeEach(function beforeEach() {
+    getSyncStatus = this.sinon.stub();
+    getSyncStatusMethod = getSyncStatusMethodFactory(getSyncStatus);
+  });
+
+  it('should throw an error if getSyncStatus fails', async () => {
+    getSyncStatus.throws(new Error());
+    expect(getSyncStatusMethod()).to.be.rejectedWith(Error);
+  });
+
+  it('should return Sync Status', async () => {
+    const lastSyncedBlock = blocks[0];
+    const lastChainBlock = blocks[3];
+    const lastSyncAt = new Date();
+    const status = 'sync';
+    const syncStatus = new SyncStatus(
+      lastSyncedBlock.height,
+      lastSyncedBlock.hash,
+      lastSyncAt,
+      lastChainBlock.height,
+      lastChainBlock.hash,
+      status,
+    );
+    getSyncStatus.returns(syncStatus);
+    const syncStatusData = await getSyncStatusMethod();
+    expect(syncStatusData).to.be.deep.equal({
+      lastSyncedBlockHeight: lastSyncedBlock.height,
+      lastSyncedBlockHash: lastSyncedBlock.hash,
+      lastSyncAt,
+      currentBlockHeight: lastChainBlock.height,
+      currentBlockHash: lastChainBlock.hash,
+      status,
+    });
+  });
+});
+
