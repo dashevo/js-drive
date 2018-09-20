b0c00fb1c00ed138895885e49daedb506533ddf6
David Rey <david.rey.acosta@gmail.com>
2018-09-20 23:17:52 +0200

    Initial sync.js refactor idea

--- scripts/sync.js
+++ scripts/sync.js
@@ -30,149 +30,183 @@ const errorHandler = require('../lib/util/errorHandler');
 const isDashCoreRunningFactory = require('../lib/sync/isDashCoreRunningFactory');
 const DashCoreIsNotRunningError = require('../lib/sync/DashCoreIsNotRunningError');
 
-(async function main() {
-  const rpcClient = new RpcClient({
-    protocol: 'http',
-    host: process.env.DASHCORE_JSON_RPC_HOST,
-    port: process.env.DASHCORE_JSON_RPC_PORT,
-    user: process.env.DASHCORE_JSON_RPC_USER,
-    pass: process.env.DASHCORE_JSON_RPC_PASS,
-  });
-
-  const isDashCoreRunning = isDashCoreRunningFactory(rpcClient);
-
-  const isRunning = await isDashCoreRunning(
-    parseInt(process.env.DASHCORE_RUNNING_CHECK_MAX_RETRIES, 10),
-    parseInt(process.env.DASHCORE_RUNNING_CHECK_INTERVAL, 10),
-  );
-  if (!isRunning) {
-    throw new DashCoreIsNotRunningError();
+class SyncApplication {
+  constructor() {
+    this.rpcClient = new RpcClient({
+      protocol: 'http',
+      host: process.env.DASHCORE_JSON_RPC_HOST,
+      port: process.env.DASHCORE_JSON_RPC_PORT,
+      user: process.env.DASHCORE_JSON_RPC_USER,
+      pass: process.env.DASHCORE_JSON_RPC_PASS,
+    });
+    this.ipfsAPI = new IpfsAPI(process.env.STORAGE_IPFS_MULTIADDR);
+    this.mongoClient = null;
+    this.syncStateRepository = null;
+    this.syncState = null;
   }
 
-  const blockIterator = new RpcBlockIterator(
-    rpcClient,
-    parseInt(process.env.SYNC_EVO_START_BLOCK_HEIGHT, 10),
-  );
+  async init() {
+    const isDashCoreRunning = isDashCoreRunningFactory(this.rpcClient);
 
-  const stHeaderIterator = new StateTransitionHeaderIterator(blockIterator, rpcClient);
+    const isRunning = await isDashCoreRunning(
+      parseInt(process.env.DASHCORE_RUNNING_CHECK_MAX_RETRIES, 10),
+      parseInt(process.env.DASHCORE_RUNNING_CHECK_INTERVAL, 10),
+    );
+    if (!isRunning) {
+      throw new DashCoreIsNotRunningError();
+    }
 
-  const mongoClient = await MongoClient.connect(
-    process.env.STORAGE_MONGODB_URL,
-    { useNewUrlParser: true },
-  );
-  const mongoDb = mongoClient.db(process.env.STORAGE_MONGODB_DB);
-  const syncStateRepository = new SyncStateRepository(mongoDb);
-  const syncState = await syncStateRepository.fetch();
+    this.mongoClient = await MongoClient.connect(
+      process.env.STORAGE_MONGODB_URL,
+      { useNewUrlParser: true },
+    );
 
-  // TODO: Parse variable to int if present
-  const stHeadersReaderState = new STHeadersReaderState(
-    syncState.getBlocks(),
-    process.env.SYNC_STATE_BLOCKS_LIMIT,
-  );
+    const mongoDb = this.mongoClient.db(process.env.STORAGE_MONGODB_DB);
+    this.syncStateRepository = new SyncStateRepository(mongoDb);
+    this.syncState = await this.syncStateRepository.fetch();
 
-  const stHeaderReader = new STHeadersReader(stHeaderIterator, stHeadersReaderState);
+    this.isInitialized = true;
+  }
 
-  const ipfsAPI = new IpfsAPI(process.env.STORAGE_IPFS_MULTIADDR);
+  getMongoClient() {
+    return this.mongoClient;
+  }
 
-  const unpinAllIpfsPackets = unpinAllIpfsPacketsFactory(ipfsAPI);
-  const dropMongoDatabasesWithPrefix = dropMongoDatabasesWithPrefixFactory(mongoClient);
-  const cleanDashDrive = cleanDashDriveFactory(unpinAllIpfsPackets, dropMongoDatabasesWithPrefix);
+  getRpcClient() {
+    return this.rpcClient;
+  }
+
+  getSyncStateRepository() {
+    return this.syncStateRepository;
+  }
+
+  getSyncState() {
+    return this.syncState;
+  }
+
+  getIpfsApi() {
+    return this.ipfsAPI;
+  }
+
+  createSTHeadersReader() {
+    const blockIterator = new RpcBlockIterator(
+      this.getRpcClient(),
+      parseInt(process.env.SYNC_EVO_START_BLOCK_HEIGHT, 10),
+    );
+    const stHeaderIterator = new StateTransitionHeaderIterator(
+      blockIterator,
+      this.getRpcClient(),
+    );
+    const stHeadersReaderState = new STHeadersReaderState(
+      this.getSyncState().getBlocks(),
+      process.env.SYNC_STATE_BLOCKS_LIMIT,
+    );
+    return new STHeadersReader(stHeaderIterator, stHeadersReaderState);
+  }
+
+  createUnpinAllIpfsPackets() {
+    return unpinAllIpfsPacketsFactory(this.getIpfsApi());
+  }
+
+  createDropMongoDatabasesWithPrefix() {
+    return dropMongoDatabasesWithPrefixFactory(this.getMongoClient());
+  }
+
+  createCleanDashDrive() {
+    return cleanDashDriveFactory(
+      this.createUnpinAllIpfsPackets(),
+      this.createDropMongoDatabasesWithPrefix(),
+    );
+  }
+
+  createApplyStateTransition() {
+    const mongoDb = this.getMongoClient().db(process.env.STORAGE_MONGODB_DB);
+    const dapContractMongoDbRepository = new DapContractMongoDbRepository(mongoDb, sanitizeData);
+    const createDapObjectMongoDbRepository = createDapObjectMongoDbRepositoryFactory(
+      this.getMongoClient(),
+      DapObjectMongoDbRepository,
+    );
+    const updateDapContract = updateDapContractFactory(dapContractMongoDbRepository);
+    const updateDapObject = updateDapObjectFactory(createDapObjectMongoDbRepository);
+    return applyStateTransitionFactory(
+      this.getIpfsApi(),
+      updateDapContract,
+      updateDapObject,
+    );
+  }
+}
+
+function runSyncFactory(stHeaderReader, syncState, cleanDashDrive) {
+  let isInSync = false;
+  async function runSync(sinceBlockHash) {
+    try {
+      if (isInSync) {
+        return;
+      }
+      isInSync = true;
+
+      // Start sync from the last synced block + 1
+      stHeaderReader.stHeaderIterator.blockIterator.setBlockHeightFromSyncState(syncState);
+      // Reset height to the current block's height
+      await stHeaderReader.stHeaderIterator.blockIterator.resetFromCurrentBlockHeight(sinceBlockHash);
+
+      stHeaderReader.stHeaderIterator.reset(false);
+
+      await stHeaderReader.read();
+
+      isInSync = false;
+    } catch (error) {
+      if (error.message === 'Block height out of range' && !syncState.isEmpty()) {
+        await cleanDashDrive(process.env.MONGODB_DB_PREFIX);
+
+        stHeaderReader.state.clear();
+        stHeaderReader.stHeaderIterator.reset(false);
+        stHeaderReader.stHeaderIterator.blockIterator.setBlockHeight(1);
+
+        syncState.setBlocks([]);
+        syncState.setLastSyncAt(null);
+        syncState.setLastInitialSyncAt(null);
+
+        isInSync = false;
+
+        await runSync();
+
+        return;
+      }
+      isInSync = false;
+      throw error;
+    }
+  }
+  return runSync;
+}
+
+(async function main() {
+  const syncApplication = new SyncApplication();
+  await syncApplication.init();
+
+  const stHeaderReader = syncApplication.createSTHeadersReader();
+  const ipfsAPI = syncApplication.getIpfsApi();
+  const unpinAllIpfsPackets = syncApplication.createUnpinAllIpfsPackets();
+  const syncState = syncApplication.getSyncState();
+  const syncStateRepository = syncApplication.getSyncStateRepository();
+  const applyStateTransition = syncApplication.createApplyStateTransition();
+  const dropMongoDatabasesWithPrefix = syncApplication.createDropMongoDatabasesWithPrefix();
+  const cleanDashDrive = syncApplication.createCleanDashDrive();
 
   attachStorageHandlers(stHeaderReader, ipfsAPI, unpinAllIpfsPackets);
   attachSyncHandlers(stHeaderReader, syncState, syncStateRepository);
-  const dapContractMongoDbRepository = new DapContractMongoDbRepository(mongoDb, sanitizeData);
-  const createDapObjectMongoDbRepository = createDapObjectMongoDbRepositoryFactory(
-    mongoClient,
-    DapObjectMongoDbRepository,
-  );
-  const updateDapContract = updateDapContractFactory(dapContractMongoDbRepository);
-  const updateDapObject = updateDapObjectFactory(createDapObjectMongoDbRepository);
-  const applyStateTransition = applyStateTransitionFactory(
-    ipfsAPI,
-    updateDapContract,
-    updateDapObject,
-  );
   attachStateViewHandlers(stHeaderReader, applyStateTransition, dropMongoDatabasesWithPrefix);
 
-  let isFirstSyncCompleted = false;
-  let isInSync = false;
-
-  async function resetDashDrive() {
-    await cleanDashDrive(process.env.MONGODB_DB_PREFIX);
-    stHeadersReaderState.clear();
-    stHeaderIterator.reset(false);
-    blockIterator.setBlockHeight(1);
-    syncState.setBlocks([]);
-    syncState.setLastSyncAt(null);
-  }
-
-  /**
-   * @param {Buffer} [sinceBlockHash]
-   * @returns {Promise<void>}
-   */
-  async function sync(sinceBlockHash = undefined) {
-    if (isInSync) {
-      return;
-    }
-
-    isInSync = true;
-
-    try {
-      // Start sync from the last synced block + 1
-      let height = blockIterator.getBlockHeight();
-      if (isFirstSyncCompleted) {
-        height += 1;
-      }
-
-      // Reset height to the current block's height
-      if (sinceBlockHash) {
-        const { result: { height: blockHeight } } = await rpcClient.getBlock(sinceBlockHash.toString('hex'));
-        if (blockHeight < height) {
-          height = blockHeight;
-        }
-      }
-
-      blockIterator.setBlockHeight(height);
-      stHeaderIterator.reset(false);
-
-      try {
-        await stHeaderReader.read();
-      } catch (error) {
-        if (error.message !== 'Block height out of range') {
-          throw error;
-        }
-
-        if (!syncState.isEmpty()) {
-          await resetDashDrive();
-
-          isInSync = false;
-          isFirstSyncCompleted = false;
-
-          await sync();
-          return;
-        }
-      }
-
-      isFirstSyncCompleted = true;
-      isInSync = false;
-    } catch (e) {
-      isInSync = false;
-
-      throw e;
-    }
-  }
-
-  await sync();
+  const runSync = runSyncFactory(stHeaderReader, syncState, cleanDashDrive);
+  await runSync();
 
   // Sync arriving ST packets
   const zmqSocket = zmq.createSocket('sub');
   zmqSocket.connect(process.env.DASHCORE_ZMQ_PUB_HASHBLOCK);
 
   zmqSocket.on('message', (topic, blockHash) => {
-    sync(blockHash).catch((error) => {
-      isInSync = false;
-      errorHandler(error);
-    });
+    const sinceBlockHash = blockHash.toString('hex');
+    runSync(sinceBlockHash).catch(errorHandler);
   });
 
   zmqSocket.subscribe('hashblock');
