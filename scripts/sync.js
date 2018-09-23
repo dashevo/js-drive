20b4738d157e00e51a0073ce200c1bf11b77467d
David Rey <david.rey.acosta@gmail.com>
2018-09-23 22:02:46 +0200

    Move SyncAppOptions to lib/app

--- scripts/sync.js
+++ scripts/sync.js
@@ -2,82 +2,13 @@ require('dotenv-expand')(require('dotenv-safe').config());
 
 const zmq = require('zeromq');
 
+const SyncAppOptions = require('../lib/app/SyncAppOptions');
 const SyncApp = require('../lib/app/SyncApp');
 const attachStorageHandlers = require('../lib/storage/attachStorageHandlers');
 const attachSyncHandlers = require('../lib/sync/state/attachSyncHandlers');
 const attachStateViewHandlers = require('../lib/stateView/attachStateViewHandlers');
 const readChainFactory = require('../lib/blockchain/readChainFactory');
 const errorHandler = require('../lib/util/errorHandler');
-
-class SyncAppOptions {
-  constructor(options) {
-    this.dashCoreJsonRpcHost = options.DASHCORE_JSON_RPC_HOST;
-    this.dashCoreJsonRpcPort = options.DASHCORE_JSON_RPC_PORT;
-    this.dashCoreJsonRpcUser = options.DASHCORE_JSON_RPC_USER;
-    this.dashCoreJsonRpcPass = options.DASHCORE_JSON_RPC_PASS;
-    this.dashCoreRunningCheckMaxRetries = parseInt(options.DASHCORE_RUNNING_CHECK_MAX_RETRIES, 10);
-    this.dashCoreRunningCheckInterval = parseInt(options.DASHCORE_RUNNING_CHECK_INTERVAL, 10);
-    this.dashCoreZmqPubHashblock = options.DASHCORE_ZMQ_PUB_HASHBLOCK;
-    this.storageIpfsMultiAddr = options.STORAGE_IPFS_MULTIADDR;
-    this.storageMongoDbUrl = options.STORAGE_MONGODB_URL;
-    this.storageMongoDbDatabase = options.STORAGE_MONGODB_DB;
-    this.syncEvoStartBlockHeight = parseInt(options.SYNC_EVO_START_BLOCK_HEIGHT, 10);
-    this.syncStateBlocksLimit = options.SYNC_STATE_BLOCKS_LIMIT;
-    this.mongoDbPrefix = options.MONGODB_DB_PREFIX;
-  }
-
-  getDashCoreJsonRpcHost() {
-    return this.dashCoreJsonRpcHost;
-  }
-
-  getDashCoreJsonRpcPort() {
-    return this.dashCoreJsonRpcPort;
-  }
-
-  getDashCoreJsonRpcUser() {
-    return this.dashCoreJsonRpcUser;
-  }
-
-  getDashCoreJsonRpcPass() {
-    return this.dashCoreJsonRpcPass;
-  }
-
-  getDashCoreRunningCheckMaxRetries() {
-    return this.dashCoreRunningCheckMaxRetries;
-  }
-
-  getDashCoreRunningCheckInterval() {
-    return this.dashCoreRunningCheckInterval;
-  }
-
-  getDashCoreZmqPubHashBlock() {
-    return this.dashCoreZmqPubHashblock;
-  }
-
-  getStorageIpfsMultiAddr() {
-    return this.storageIpfsMultiAddr;
-  }
-
-  getStorageMongoDbUrl() {
-    return this.storageMongoDbUrl;
-  }
-
-  getStorageMongoDbDatabase() {
-    return this.storageMongoDbDatabase;
-  }
-
-  getSyncEvoStartBlockHeight() {
-    return this.syncEvoStartBlockHeight;
-  }
-
-  getSyncStateBlocksLimit() {
-    return this.syncStateBlocksLimit;
-  }
-
-  getMongoDbPrefix() {
-    return this.mongoDbPrefix;
-  }
-}
 
 (async function main() {
   const syncAppOptions = new SyncAppOptions(process.env);
