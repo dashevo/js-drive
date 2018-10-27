b09223225a57e9b756d8ac134cd63a88b705928b
Ivan Shumkov <ivan@shumkov.ru>
2018-10-27 21:22:08 +0100

    Add specs for `readBlockChain` test

--- test/unit/blockchain/reader/readBlockchainFactory.js
+++ test/unit/blockchain/reader/readBlockchainFactory.js
@@ -1,3 +1,8 @@
 describe('readBlockchainFactory', () => {
+  it('should emit the out of bounds event if initial block height less than number of blocks in blockchain');
+  it('should reset state and emit out of bounds events if initial block height less and Drive synced something before');
+  it('should emit the fully synced event if the last synced block and the last block from the chain are the same');
+  it('should read from the blockchain height if it less than the last sync block');
+  it('should read from the next block after the last synced block');
   it('should read from initial block height');
 });
