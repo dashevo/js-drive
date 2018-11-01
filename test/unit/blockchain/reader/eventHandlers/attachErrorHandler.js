961da81e3f25b754c01d3e3330f4deb790398bb8
Djavid Gabibiyan <djavidoff@gmail.com>
2018-11-01 12:02:43 +0500

    remove unnecessary `describe` section for `attachErrorHandler` test

--- test/unit/blockchain/reader/eventHandlers/attachErrorHandler.js
+++ test/unit/blockchain/reader/eventHandlers/attachErrorHandler.js
@@ -20,58 +20,56 @@ describe('attachErrorHandler', () => {
     stateTransitions = getTransitionHeaderFixtures();
   });
 
-  describe('State Transition error handler', () => {
-    it('should do nothing if skipStateTransitionWithErrors is disabled', async () => {
-      attachErrorHandler(
-        readerMediatorMock,
-        {
-          skipStateTransitionWithErrors: false,
-        },
-      );
+  it('should do nothing if skipStateTransitionWithErrors is disabled', async () => {
+    attachErrorHandler(
+      readerMediatorMock,
+      {
+        skipStateTransitionWithErrors: false,
+      },
+    );
 
+    await readerMediatorMock.originalEmitSerial(
+      ReaderMediator.EVENTS.STATE_TRANSITION_ERROR,
+      {
+        block: blocks[0],
+        stateTransition: stateTransitions[0],
+      },
+    );
+
+    expect(readerMediatorMock.emitSerial).to.be.not.called();
+  });
+
+  it('should skip State Transition if skipStateTransitionWithErrors is enabled', async () => {
+    const [stateTransition] = stateTransitions;
+
+    attachErrorHandler(
+      readerMediatorMock,
+      {
+        skipStateTransitionWithErrors: true,
+      },
+    );
+
+    let expectedError;
+    try {
       await readerMediatorMock.originalEmitSerial(
         ReaderMediator.EVENTS.STATE_TRANSITION_ERROR,
-        {
-          block: blocks[0],
-          stateTransition: stateTransitions[0],
-        },
-      );
-
-      expect(readerMediatorMock.emitSerial).to.be.not.called();
-    });
-
-    it('should skip State Transition if skipStateTransitionWithErrors is enabled', async () => {
-      const [stateTransition] = stateTransitions;
-
-      attachErrorHandler(
-        readerMediatorMock,
-        {
-          skipStateTransitionWithErrors: true,
-        },
-      );
-
-      let expectedError;
-      try {
-        await readerMediatorMock.originalEmitSerial(
-          ReaderMediator.EVENTS.STATE_TRANSITION_ERROR,
-          {
-            block: blocks[0],
-            stateTransition,
-          },
-        );
-      } catch (e) {
-        expectedError = e;
-      }
-
-      expect(expectedError).to.be.instanceOf(IgnoreStateTransitionError);
-
-      expect(readerMediatorMock.emitSerial).to.be.calledOnceWith(
-        ReaderMediator.EVENTS.STATE_TRANSITION_SKIP,
         {
           block: blocks[0],
           stateTransition,
         },
       );
-    });
+    } catch (e) {
+      expectedError = e;
+    }
+
+    expect(expectedError).to.be.instanceOf(IgnoreStateTransitionError);
+
+    expect(readerMediatorMock.emitSerial).to.be.calledOnceWith(
+      ReaderMediator.EVENTS.STATE_TRANSITION_SKIP,
+      {
+        block: blocks[0],
+        stateTransition,
+      },
+    );
   });
 });
