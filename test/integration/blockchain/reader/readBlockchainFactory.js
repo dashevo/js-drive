8498e35547e7b156b9c79368a30419701e2af598
Djavid Gabibiyan <djavidoff@gmail.com>
2018-11-23 02:09:54 +0500

    add pending integration tests for readBlockchainFactory

---
+++ test/integration/blockchain/reader/readBlockchainFactory.js
@@ -0,0 +1,8 @@
+describe('readBlockchainFactory', () => {
+  it('should initially read whole chain available');
+  it('should emit out of bounds event if chain is behind');
+  it('should reset state in case it is not empty and chain is behind');
+  it('should emit fully synced event in case it has state and last block hash matches one on the blockchain');
+  it('should check for impossibility of sequence validation and reset state if any');
+  it('should proceed to the next block if chain has one');
+});
