0592f888bd8b1a18e74cfb63a5534591c7e87a6b
Djavid Gabibiyan <djavidoff@gmail.com>
2018-11-23 19:03:21 +0500

    add two more cases for blockchainReader integration test

--- test/integration/blockchain/reader/readBlockchainFactory.js
+++ test/integration/blockchain/reader/readBlockchainFactory.js
@@ -5,4 +5,6 @@ describe('readBlockchainFactory', () => {
   it('should emit fully synced event in case it has state and last block hash matches one on the blockchain');
   it('should check for impossibility of sequence validation and reset state if any');
   it('should proceed to the next block if chain has one');
+  it('should restart reading blockchain in case sequence validation is impossible');
+  it('should throw an error and stop syncing in case unknown error is thrown during read');
 });
