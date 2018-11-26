9be3164dd052ab464476398c2b9da981ae340c3b
Djavid Gabibiyan <djavidoff@gmail.com>
2018-11-26 20:27:04 +0500

    a little bit more comments

--- test/e2e/sync/blockchainReorganization.js
+++ test/e2e/sync/blockchainReorganization.js
@@ -181,6 +181,7 @@ describe('Blockchain reorganization', function main() {
     await dashDriveSyncToFinish(secondDashDrive.driveApi);
 
     // Check data is on both Drive nodes
+    // Check data on first node
     const { result: firstDriveFirstContract } = await firstDashDrive.driveApi.getApi()
       .request('fetchDapContract', { dapId: firstDapId });
 
@@ -190,6 +191,7 @@ describe('Blockchain reorganization', function main() {
     expect(firstDriveFirstContract).to.be.deep.equal(firstContractPacket.dapcontract);
     expect(firstDriveFirstObject).to.be.deep.equal(firstObjectPacket.dapobjects[0]);
 
+    // Check data on the second node
     const { result: secondDriveFirstContract } = await secondDashDrive.driveApi.getApi()
       .request('fetchDapContract', { dapId: firstDapId });
 
@@ -225,6 +227,7 @@ describe('Blockchain reorganization', function main() {
 
     await dashDriveSyncToFinish(firstDashDrive.driveApi);
 
+    // Check second contract and object is created on the first node
     const { result: firstDriveSecondContract } = await firstDashDrive.driveApi.getApi()
       .request('fetchDapContract', { dapId: secondDapId });
 
@@ -235,6 +238,7 @@ describe('Blockchain reorganization', function main() {
     expect(firstDriveSecondObject).to.be.deep.equal(secondObjectPacket.dapobjects[0]);
 
     // Generate 2 more blocks, 3rd contract and object on the second Drive node
+    // To introduce a slightly bigger fork
     await secondDashDrive.dashCore.getApi().generate(2);
 
     const thirdContractTxId = await createAndSubmitST(
@@ -259,6 +263,7 @@ describe('Blockchain reorganization', function main() {
 
     await dashDriveSyncToFinish(secondDashDrive.driveApi);
 
+    // Check third contract and object are created on the second node
     const { result: secondDriveThirdContract } = await secondDashDrive.driveApi.getApi()
       .request('fetchDapContract', { dapId: thirdDapId });
 
@@ -270,6 +275,8 @@ describe('Blockchain reorganization', function main() {
   });
 
   it('Dash Drive should sync data after blockchain reorganization, removing missing STs. Adding them back after they reappear in the blockchain.', async () => {
+    // Save initialSyncAt to test it later and make sure
+    // There was no intial sync
     const {
       result: {
         lastInitialSyncAt: lastInitialSyncAtBefore,
@@ -309,7 +316,7 @@ describe('Blockchain reorganization', function main() {
     expect(secondDriveSecondContract).to.be.undefined();
     expect(secondDriveSecondObject).to.be.undefined();
 
-    // Generate more blocks
+    // Generate more blocks so transitions are back from mempool
     await firstDashDrive.dashCore.getApi().generate(5);
 
     // Await block count to be equal on both nodes
