27a218156da68fa2d2e580280ed9d01fce5c9017
Djavid Gabibiyan <djavidoff@gmail.com>
2018-11-29 16:34:34 +0500

    integration test for dap object events

--- test/integration/stateView/dapObject/revertDapObjectsForStateTransitionFactory.js
+++ test/integration/stateView/dapObject/revertDapObjectsForStateTransitionFactory.js
@@ -5,9 +5,12 @@ const {
   },
 } = require('@dashevo/js-evo-services-ctl');
 
+const ReaderMediator = require('../../../../lib/blockchain/reader/BlockchainReaderMediator');
+
 const Reference = require('../../../../lib/stateView/Reference');
 const DapObjectMongoDbRepository = require('../../../../lib/stateView/dapObject/DapObjectMongoDbRepository');
 const DapObject = require('../../../../lib/stateView/dapObject/DapObject');
+const generateDapObjectId = require('../../../../lib/stateView/dapObject/generateDapObjectId');
 
 const revertDapObjectsForStateTransitionFactory = require('../../../../lib/stateView/dapObject/revertDapObjectsForStateTransitionFactory');
 const createDapObjectMongoDbRepositoryFactory = require('../../../../lib/stateView/dapObject/createDapObjectMongoDbRepositoryFactory');
@@ -42,6 +45,7 @@ describe('revertDapObjectsForStateTransitionFactory', () => {
   let updateDapObject;
   let applyStateTransition;
   let rpcClientMock;
+  let readerMediator;
   let revertDapObjectsForStateTransition;
   beforeEach(function beforeEach() {
     addSTPacket = addSTPacketFactory(ipfsAPI);
@@ -50,7 +54,7 @@ describe('revertDapObjectsForStateTransitionFactory', () => {
       DapObjectMongoDbRepository,
     );
     updateDapObject = updateDapObjectFactory(createDapObjectMongoDbRepository);
-    const readerMediator = new ReaderMediatorMock(this.sinon);
+    readerMediator = new ReaderMediatorMock(this.sinon);
     applyStateTransition = applyStateTransitionFactory(
       ipfsAPI,
       null,
@@ -86,12 +90,7 @@ describe('revertDapObjectsForStateTransitionFactory', () => {
       packet.dapid,
     );
 
-    const dapObjectData = {
-      objtype: 'user',
-      idx: 0,
-      rev: 1,
-      act: 0,
-    };
+    const [dapObjectData] = packet.dapobjects;
 
     const reference = new Reference(
       block.hash,
@@ -110,6 +109,16 @@ describe('revertDapObjectsForStateTransitionFactory', () => {
     const dapObjectList = await dapObjectRepository.fetch('user');
 
     expect(dapObjectList).to.be.empty();
+
+    expect(readerMediator.emitSerial).to.be.calledWith(
+      ReaderMediator.EVENTS.DAP_OBJECT_MARKED_DELETED,
+      {
+        userId: transition.extraPayload.regTxId,
+        objectId: generateDapObjectId(blockchainUserId, dapObjectData.idx),
+        reference,
+        object: dapObjectData,
+      },
+    );
   });
 
   it('should revert Dap Object to its previous revision if any', async () => {
@@ -124,11 +133,11 @@ describe('revertDapObjectsForStateTransitionFactory', () => {
     const references = [];
 
     let lastTransition;
+    const [dapObjectData] = packet.dapobjects;
     for (let i = 0; i < 3; i++) {
       const block = blocks[i];
       const transition = transitions[i];
 
-      const [dapObjectData] = packet.dapobjects;
       dapObjectData.act = (i === 0 ? DapObject.ACTION_CREATE : DapObject.ACTION_UPDATE);
       dapObjectData.rev = i + 1;
 
@@ -174,6 +183,20 @@ describe('revertDapObjectsForStateTransitionFactory', () => {
 
     expect(previousRevision.revision).to.be.equal(1);
     expect(previousRevision.reference).to.be.deep.equal(references[0]);
+
+    expect(readerMediator.emitSerial.getCall(2)).to.be.calledWith(
+      ReaderMediator.EVENTS.DAP_OBJECT_REVERTED,
+      {
+        userId: lastTransition.extraPayload.regTxId,
+        objectId: generateDapObjectId(blockchainUserId, dapObjectData.idx),
+        reference: references[references.length - 1],
+        object: dapObjectData,
+        previousRevision: {
+          reference: references[references.length - 2],
+          revision: references.length - 1,
+        },
+      },
+    );
   });
 
   it('should not do anything if packet have no Dap ID');
