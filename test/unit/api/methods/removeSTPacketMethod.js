17e21146e77213c8f1c8304b44a1d3ef6a01851f
Djavid Gabibiyan <djavidoff@gmail.com>
2018-12-26 21:25:52 +0500

    catch PacketNotPinnedError in removeSTPacketMethod

--- test/unit/api/methods/removeSTPacketMethod.js
+++ test/unit/api/methods/removeSTPacketMethod.js
@@ -1,5 +1,6 @@
 const proxyquire = require('proxyquire');
 
+const PacketNotPinnedError = require('../../../../lib/storage/errors/PacketNotPinnedError');
 const CIDCreationFailedError = require('../../../../lib/storage/errors/CIDCreationFailedError');
 const InvalidParamsError = require('../../../../lib/api/InvalidParamsError');
 
@@ -37,6 +38,19 @@ describe('removeSTPacketMethod', () => {
   it('should throw an original error if error is not CIDCreationFailedError', () => {
     const error = new Error('Some unknown error');
     stateTransitonPacketMock.createCIDFromHash.throws(error);
+    expect(removeSTPacketMethod({ packetHash: 'other' })).to.be.rejectedWith(error);
+  });
+
+  it('should throw error if packet is not pinned', () => {
+    stateTransitonPacketMock.createCIDFromHash.returns({});
+    removeSTPacket.throws(new PacketNotPinnedError());
+    expect(removeSTPacketMethod({ packetHash: 'non_existent' })).to.be.rejectedWith(InvalidParamsError);
+  });
+
+  it('should throw an original error if error is not PacketNotPinnedError', () => {
+    const error = new Error('Some unknown error');
+    stateTransitonPacketMock.createCIDFromHash.returns({});
+    removeSTPacket.throws(error);
     expect(removeSTPacketMethod({ packetHash: 'other' })).to.be.rejectedWith(error);
   });
 
