22e548d9c5181a20a7bb829fbf18d0c1f602a412
Djavid Gabibiyan <djavidoff@gmail.com>
2018-12-26 14:29:10 +0500

    accept packetHash instead of CID in removeSTPacketMethod

--- test/unit/api/methods/removeSTPacketMethod.js
+++ test/unit/api/methods/removeSTPacketMethod.js
@@ -1,31 +1,45 @@
-const CID = require('cids');
+const proxyquire = require('proxyquire');
 
-const removeSTPacketMethodFactory = require('../../../../lib/api/methods/removeSTPacketMethodFactory');
 const InvalidParamsError = require('../../../../lib/api/InvalidParamsError');
 
 describe('removeSTPacketMethod', () => {
   let removeSTPacket;
   let removeSTPacketMethod;
+  let stateTransitonPacketMock;
   beforeEach(function beforeEach() {
+    stateTransitonPacketMock = {
+      createCIDFromHash: this.sinon.stub(),
+    };
+
+    const removeSTPacketMethodFactory = proxyquire(
+      '../../../../lib/api/methods/removeSTPacketMethodFactory',
+      {
+        '../../storage/stPacket/StateTransitionPacket': stateTransitonPacketMock,
+      },
+    );
+
     removeSTPacket = this.sinon.stub();
     removeSTPacketMethod = removeSTPacketMethodFactory(removeSTPacket);
   });
 
-  it('should throw error if "cid" params is missing', () => {
+  it('should throw error if "packetHash" params is missing', () => {
     expect(removeSTPacketMethod({ })).to.be.rejectedWith(InvalidParamsError);
     expect(removeSTPacket).to.not.be.called();
   });
 
-  it('should throw error if "cid" params is not a valid CID hash', () => {
-    expect(removeSTPacketMethod({ cid: 'wrong' })).to.be.rejectedWith(InvalidParamsError);
+  it('should throw error if "packetHash" params is not a valid CID hash', () => {
+    stateTransitonPacketMock.createCIDFromHash.throws(new InvalidParamsError());
+    expect(removeSTPacketMethod({ packetHash: 'wrong' })).to.be.rejectedWith(InvalidParamsError);
     expect(removeSTPacket).to.not.be.called();
   });
 
   it('should delete ST Packet', async () => {
-    const cidHash = 'zdrSLCCKBrU4ma3BQA3gcjjXLRbBiYk38cocCGfFbcvRZdsMu';
-    const cid = new CID(cidHash);
+    const cid = {
+      value: 'some_potential_cid',
+    };
+    stateTransitonPacketMock.createCIDFromHash.returns(cid);
 
-    await removeSTPacketMethod({ cid: cidHash });
+    await removeSTPacketMethod({ packetHash: 'some_packet_hash' });
 
     expect(removeSTPacket).to.be.calledOnce();
     expect(removeSTPacket).to.be.calledWith(cid);
