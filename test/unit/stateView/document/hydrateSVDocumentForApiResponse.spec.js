327db2ea822892b3ed49727532f918880f0bb772
Djavid Gabibiyan <djavidoff@gmail.com>
2019-04-26 20:19:27 +0500

    Use `getSVDocumentsFixture`

--- test/unit/stateView/document/hydrateSVDocumentForApiResponse.spec.js
+++ test/unit/stateView/document/hydrateSVDocumentForApiResponse.spec.js
@@ -1,38 +1,20 @@
-const SVDocument = require('../../../../lib/stateView/document/SVDocument');
-
-const getDocumentsFixture = require('../../../../lib/test/fixtures/getDocumentsFixture');
-const getReferenceFixture = require('../../../../lib/test/fixtures/getReferenceFixture');
+const getSVDocumentsFixture = require('../../../../lib/test/fixtures/getSVDocumentsFixture');
 
 const hydrateSVDocumentForApiResponse = require('../../../../lib/stateView/document/hydrateSVDocumentForApiResponse');
 
 describe('hydrateSVDocumentForApiResponse', () => {
   let svDocument;
-  let userId;
-  let document;
-  let reference;
-  let isDeleted;
-  let previousRevisions;
 
   before(() => {
-    ({ userId } = getDocumentsFixture);
-    [document] = getDocumentsFixture();
-    reference = getReferenceFixture();
-    isDeleted = false;
-    previousRevisions = [];
-
-    svDocument = new SVDocument(
-      userId,
-      document,
-      reference,
-      isDeleted,
-      previousRevisions,
-    );
+    [svDocument] = getSVDocumentsFixture();
   });
 
   it('should produce the right presentation of SVDocument for the API response', () => {
     const hydrated = hydrateSVDocumentForApiResponse(svDocument);
 
-    document.setMeta('userId', userId);
+    const document = svDocument.getDocument();
+
+    document.setMeta('userId', svDocument.getUserId());
 
     expect(hydrated).to.deep.equal(document);
   });
--- test/unit/stateView/document/hydrateSVDocumentForDB.spec.js
+++ test/unit/stateView/document/hydrateSVDocumentForDB.spec.js
@@ -1,32 +1,12 @@
-const SVDocument = require('../../../../lib/stateView/document/SVDocument');
-
-const getDocumentsFixture = require('../../../../lib/test/fixtures/getDocumentsFixture');
-const getReferenceFixture = require('../../../../lib/test/fixtures/getReferenceFixture');
+const getSVDocumentsFixture = require('../../../../lib/test/fixtures/getSVDocumentsFixture');
 
 const hydrateSVDocumentForDB = require('../../../../lib/stateView/document/hydrateSVDocumentForDB');
 
 describe('hydrateSVDocumentForDB', () => {
   let svDocument;
-  let userId;
-  let document;
-  let reference;
-  let isDeleted;
-  let previousRevisions;
 
   before(() => {
-    ({ userId } = getDocumentsFixture);
-    [document] = getDocumentsFixture();
-    reference = getReferenceFixture();
-    isDeleted = false;
-    previousRevisions = [];
-
-    svDocument = new SVDocument(
-      userId,
-      document,
-      reference,
-      isDeleted,
-      previousRevisions,
-    );
+    [svDocument] = getSVDocumentsFixture();
   });
 
   it('should produce the right presentation of SVDocument for the database', () => {
