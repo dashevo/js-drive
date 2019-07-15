cae3b54f80854774ad1e199f76b1b3b091ea3b7b
Anton Suprunchuk <antouhou@gmail.com>
2019-07-15 15:36:42 +0300

    Cover `findConflictingConditions` with tests

--- test/unit/stateView/document/query/findConflictingConditions.spec.js
+++ test/unit/stateView/document/query/findConflictingConditions.spec.js
@@ -1,21 +1,49 @@
 const findConflictingConditions = require('../../../../../lib/stateView/document/query/findConflictingConditions');
 
+const allowedPairsTestCases = [
+  ['<', '>'],
+  ['<', '>='],
+  ['>', '<'],
+  ['>', '<='],
+];
+const restrictedPairsTestCases = [
+  ['==', '<'],
+  ['<', '<'],
+  ['>', '>'],
+  ['=>', '<='],
+  ['==', '=='],
+];
+
 describe('findConflictingConditions', () => {
-  it('should return empty array if field used with only one operator', () => {
+  it('should return an empty array if field used with only one operator', () => {
     const result = findConflictingConditions([['field', '==', 'value'], ['field2', '<', 'value2']]);
 
     expect(result).to.be.an('array');
     expect(result).to.have.lengthOf(0);
   });
 
-  it('should return empty array if field used with "<" and ">" operators');
-  it('should return empty array if field used with "<" and ">=" operators');
-  it('should return empty array if field used with ">" and "<" operators');
-  it('should return empty array if field used with ">" and "<=" operators');
-  it('should return array with field if it used with "<" and "<" operators');
-  it('should return array with field if it used with ">" and ">" operators');
-  it('should return array with field if it used with "<=" and "=>" operators');
-  it('should return array with field if it used with two not range comparison operators');
-  it('should return array with field if it used with more than two operators');
-  it('should return array with field if it used with more than two operators');
+  allowedPairsTestCases.forEach(([operator1, operator2]) => {
+    it(`should return an empty array if field used with "${operator1}" and "${operator2}" operators`, () => {
+      const result = findConflictingConditions([['a', operator1, 1], ['a', operator2, 1]]);
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.lengthOf(0);
+    });
+  });
+
+  restrictedPairsTestCases.forEach(([operator1, operator2]) => {
+    it(`should return an array with a field if field used with "${operator1}" and "${operator2}" operators`, () => {
+      const result = findConflictingConditions([['a', operator1, 1], ['a', operator2, 1]]);
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.lengthOf(1);
+    });
+  });
+
+  it('should return array with field if it used with more than two operators', () => {
+    const result = findConflictingConditions([['a', '>', 1], ['a', '<', 1], ['a', '=>', 1]]);
+
+    expect(result).to.be.an('array');
+    expect(result).to.have.lengthOf(1);
+  });
 });
