107eccfbf98d7a1f4aff9808b0fea1384ab6741b
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2019-08-28 19:13:04 +0300

    Remove ipfs stuff

--- test/integration/stateView/document/revertSVDocumentsForStateTransitionFactory.spec.js
+++ test/integration/stateView/document/revertSVDocumentsForStateTransitionFactory.spec.js
@@ -1,14 +1,10 @@
 const {
   mocha: {
     startMongoDb,
-    startIPFS,
   },
 } = require('@dashevo/dp-services-ctl');
 
-const DashPlatformProtocol = require('@dashevo/dpp');
 const Document = require('@dashevo/dpp/lib/document/Document');
-
-const ReaderMediator = require('../../../../lib/blockchain/reader/BlockchainReaderMediator');
 
 const Revision = require('../../../../lib/stateView/revisions/Revision');
 const Reference = require('../../../../lib/stateView/revisions/Reference');
@@ -24,57 +20,27 @@ const updateSVDocumentFactory = require('../../../../lib/stateView/document/upda
 const applyStateTransitionFactory = require('../../../../lib/stateView/applyStateTransitionFactory');
 const applyStateTransitionFromReferenceFactory = require('../../../../lib/stateView/applyStateTransitionFromReferenceFactory');
 
-const STPacketIpfsRepository = require('../../../../lib/storage/stPacket/STPacketIpfsRepository');
-
-const RpcClientMock = require('../../../../lib/test/mock/RpcClientMock');
-const ReaderMediatorMock = require('../../../../lib/test/mock/BlockchainReaderMediatorMock');
-
 const getBlocksFixture = require('../../../../lib/test/fixtures/getBlocksFixture');
 const getStateTransitionsFixture = require('../../../../lib/test/fixtures/getStateTransitionsFixture');
 const getSTPacketsFixture = require('../../../../lib/test/fixtures/getSTPacketsFixture');
-const getContractFixture = require('../../../../lib/test/fixtures/getContractFixture');
 
 describe('revertSVDocumentsForStateTransitionFactory', () => {
   let userId;
-  let stPacketRepository;
   let createSVDocumentMongoDbRepository;
   let updateSVDocument;
   let applyStateTransition;
-  let rpcClientMock;
-  let readerMediatorMock;
   let revertSVDocumentsForStateTransition;
   let mongoClient;
-  let ipfsAPI;
   let stPacket;
 
   startMongoDb().then((mongoDb) => {
     mongoClient = mongoDb.getClient();
   });
 
-  startIPFS().then((ipfs) => {
-    ipfsAPI = ipfs.getApi();
-  });
-
-  beforeEach(function beforeEach() {
+  beforeEach(() => {
     userId = '3557b9a8dfcc1ef9674b50d8d232e0e3e9020f49fa44f89cace622a01f43d03e';
 
     [, stPacket] = getSTPacketsFixture();
-
-    const contract = getContractFixture();
-
-    const dataProviderMock = {
-      fetchContract: this.sinon.stub().returns(contract),
-    };
-
-    const dpp = new DashPlatformProtocol({
-      dataProvider: dataProviderMock,
-    });
-
-    stPacketRepository = new STPacketIpfsRepository(
-      ipfsAPI,
-      dpp,
-      1000,
-    );
 
     const validateQuery = validateQueryFactory(findConflictingConditions);
 
@@ -87,29 +53,19 @@ describe('revertSVDocumentsForStateTransitionFactory', () => {
 
     updateSVDocument = updateSVDocumentFactory(createSVDocumentMongoDbRepository);
 
-    readerMediatorMock = new ReaderMediatorMock(this.sinon);
-
     applyStateTransition = applyStateTransitionFactory(
-      stPacketRepository,
       null,
       updateSVDocument,
-      readerMediatorMock,
     );
-
-    rpcClientMock = new RpcClientMock(this.sinon);
 
     const applyStateTransitionFromReference = applyStateTransitionFromReferenceFactory(
       applyStateTransition,
-      rpcClientMock,
     );
 
     revertSVDocumentsForStateTransition = revertSVDocumentsForStateTransitionFactory(
-      stPacketRepository,
-      rpcClientMock,
       createSVDocumentMongoDbRepository,
       applyStateTransition,
       applyStateTransitionFromReference,
-      readerMediatorMock,
     );
   });
 
@@ -120,8 +76,6 @@ describe('revertSVDocumentsForStateTransitionFactory', () => {
 
     stateTransition.extraPayload.regTxId = userId;
     stateTransition.extraPayload.hashSTPacket = stPacket.hash();
-
-    await stPacketRepository.store(stPacket);
 
     const svDocumentRepository = createSVDocumentMongoDbRepository(
       stPacket.getContractId(),
@@ -165,22 +119,12 @@ describe('revertSVDocumentsForStateTransitionFactory', () => {
         stPacketHash: reference.getSTPacketHash(),
       },
     };
-
-    expect(readerMediatorMock.emitSerial).to.have.been.calledWith(
-      ReaderMediator.EVENTS.DOCUMENT_MARKED_DELETED,
-      {
-        userId,
-        documentId: document.getId(),
-        reference,
-        document: documentJson,
-      },
-    );
   });
 
   it('should revert SVDocument to its previous revision if any', async () => {
     // TODO Revert several documents
 
-    // 1. Store 3 revisions of Document in IPFS
+    // 1. Store 3 revisions of Document
     const documentRevisions = [];
 
     const blocks = getBlocksFixture();
@@ -202,8 +146,6 @@ describe('revertSVDocumentsForStateTransitionFactory', () => {
 
       stPacket.setDocuments([updatedDocument]);
 
-      await stPacketRepository.store(stPacket);
-
       stateTransition.extraPayload.regTxId = userId;
       stateTransition.extraPayload.hashSTPacket = stPacket.hash();
 
@@ -223,12 +165,6 @@ describe('revertSVDocumentsForStateTransitionFactory', () => {
         stPacket,
         reference,
       });
-
-      rpcClientMock.getRawTransaction
-        .withArgs(stateTransition.hash)
-        .resolves({
-          result: stateTransition,
-        });
     }
 
     // 2. Create ans store SVDocument
@@ -287,17 +223,6 @@ describe('revertSVDocumentsForStateTransitionFactory', () => {
         stPacketHash: currentReference.getSTPacketHash(),
       },
     };
-
-    expect(readerMediatorMock.emitSerial.getCall(1)).to.have.been.calledWith(
-      ReaderMediator.EVENTS.DOCUMENT_REVERTED,
-      {
-        userId: svDocument.getUserId(),
-        documentId: svDocument.getDocument().getId(),
-        reference: svDocument.getReference(),
-        document: documentJson,
-        previousRevision: previousRevisions[1],
-      },
-    );
   });
 
   it('should not do anything if packet have no Contract ID');
