aa8be93adb9e705399094ad7fbca9b187d384b80
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2019-09-11 20:24:48 +0300

    Implemented `updateStateApi` script Implemented gRpc handlers skeleton

---
+++ scripts/updateStateApi.js
@@ -0,0 +1,24 @@
+require('dotenv-expand')(require('dotenv-safe').config());
+
+const grpc = require('grpc');
+
+const UpdateStateApiApp = require('../lib/app/UpdateStateApiApp');
+const UpdateStateApiAppOptions = require('../lib/app/UpdateStateApiAppOptions');
+
+const createServer = require('../lib/grpcApi/createServer');
+const errorHandler = require('../lib/util/errorHandler');
+
+(async function main() {
+  const updateStateApiAppOptions = new UpdateStateApiAppOptions(process.env);
+  const updateStateApiApp = new UpdateStateApiApp(updateStateApiAppOptions);
+
+  await updateStateApiApp.init();
+
+  const grpcServer = createServer('UpdateState', updateStateApiApp.createHandlers());
+  grpcServer.bind(
+    `${updateStateApiAppOptions.getGrpcHost()}:${updateStateApiAppOptions.getGrpcPort()}`,
+    grpc.ServerCredentials.createInsecure(),
+  );
+
+  grpcServer.start();
+}()).catch(errorHandler);
