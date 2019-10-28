61c7d4f15c51776748c9660cef8a1ee09a2c1b5a
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2019-10-28 20:14:13 +0300

    Allow using only indexed fields in fetchDocument query

--- test/integration/stateView/document/fetchDocumentsFactory.spec.js
+++ test/integration/stateView/document/fetchDocumentsFactory.spec.js
@@ -1,14 +1,19 @@
 const { mocha: { startMongoDb } } = require('@dashevo/dp-services-ctl');
 
+const DashPlatformProtocol = require('@dashevo/dpp');
+
 const SVDocumentMongoDbRepository = require('../../../../lib/stateView/document/mongoDbRepository/SVDocumentMongoDbRepository');
 const convertWhereToMongoDbQuery = require('../../../../lib/stateView/document/mongoDbRepository/convertWhereToMongoDbQuery');
 const validateQueryFactory = require('../../../../lib/stateView/document/query/validateQueryFactory');
 const findConflictingConditions = require('../../../../lib/stateView/document/query/findConflictingConditions');
+const InvalidQueryError = require('../../../../lib/stateView/document/errors/InvalidQueryError');
 
 const createSVDocumentMongoDbRepositoryFactory = require('../../../../lib/stateView/document/mongoDbRepository/createSVDocumentMongoDbRepositoryFactory');
 const fetchDocumentsFactory = require('../../../../lib/stateView/document/fetchDocumentsFactory');
+const SVContractMongoDbRepository = require('../../../../lib/stateView/contract/SVContractMongoDbRepository');
 
 const getSVDocumentsFixture = require('../../../../lib/test/fixtures/getSVDocumentsFixture');
+const getSVContractFixture = require('../../../../lib/test/fixtures/getSVContractFixture');
 
 describe('fetchDocumentsFactory', () => {
   let createSVDocumentMongoDbRepository;
@@ -18,12 +23,14 @@ describe('fetchDocumentsFactory', () => {
   let type;
   let contractId;
   let document;
+  let svContractMongoDbRepository;
+  let svContract;
 
   startMongoDb().then((mongoDb) => {
     mongoClient = mongoDb.getClient();
   });
 
-  beforeEach(() => {
+  beforeEach(async () => {
     const validateQuery = validateQueryFactory(findConflictingConditions);
 
     createSVDocumentMongoDbRepository = createSVDocumentMongoDbRepositoryFactory(
@@ -33,13 +40,28 @@ describe('fetchDocumentsFactory', () => {
       validateQuery,
     );
 
-    fetchDocuments = fetchDocumentsFactory(createSVDocumentMongoDbRepository);
+    const mongo = mongoClient.db('test');
+
+    svContractMongoDbRepository = new SVContractMongoDbRepository(
+      mongo,
+      new DashPlatformProtocol(),
+    );
+
+    fetchDocuments = fetchDocumentsFactory(
+      createSVDocumentMongoDbRepository,
+      svContractMongoDbRepository,
+    );
+
+    svContract = getSVContractFixture();
+
+    contractId = svContract.getId();
 
     [svDocument] = getSVDocumentsFixture();
 
     document = svDocument.getDocument();
     type = document.getType();
-    contractId = Buffer.alloc(32, 'somePool').toString('hex');
+
+    await svContractMongoDbRepository.store(svContract);
   });
 
   it('should fetch Documents for specified contract ID and document type', async () => {
@@ -112,5 +134,23 @@ describe('fetchDocumentsFactory', () => {
     const result = await fetchDocuments(contractId, type);
 
     expect(result).to.deep.equal([]);
+  });
+
+  it('should throw InvalidQueryError if searching by non indexed fields', async () => {
+    const svDocumentRepository = createSVDocumentMongoDbRepository(contractId, type);
+    await svDocumentRepository.store(svDocument);
+
+    const query = { where: [['lastName', '==', 'unknown']] };
+
+    try {
+      await fetchDocuments(contractId, type, query);
+
+      expect.fail('should throw InvalidQueryError');
+    } catch (e) {
+      expect(e).to.be.instanceOf(InvalidQueryError);
+      expect(e.getErrors()).to.be.an('array');
+      expect(e.getErrors()).to.have.lengthOf(1);
+      expect(e.getErrors()[0].message).to.be.equal('Search fields can only contain one of these fields: name, $id');
+    }
   });
 });
---
+++ test/unit/stateView/document/query/validateIndexedFields.spec.js
@@ -0,0 +1,67 @@
+const validateIndexedFields = require('../../../../../lib/stateView/document/query/validateIndexedFields.js');
+const ValidationResult = require('../../../../../lib/stateView/document/query/ValidationResult');
+const ValidationError = require('../../../../../lib/stateView/document/query/errors/ValidationError');
+
+describe('validateIndexedFields', () => {
+  let indexedFields;
+
+  beforeEach(() => {
+    indexedFields = ['$userId', 'firstName', 'lastName', '$id'];
+  });
+
+  it('should pass system $id field', () => {
+    const query = { where: [['$id', '==', 123]] };
+    const result = validateIndexedFields(indexedFields, query);
+
+    expect(result).to.be.an.instanceOf(ValidationResult);
+    expect(result.isValid()).to.be.true();
+    expect(result.getErrors()).to.be.an('array');
+    expect(result.getErrors()).to.have.lengthOf(0);
+  });
+
+  it('should pass', () => {
+    const query = { where: [['firstName', '==', 'name']] };
+    const result = validateIndexedFields(indexedFields, query);
+
+    expect(result).to.be.an.instanceOf(ValidationResult);
+    expect(result.isValid()).to.be.true();
+    expect(result.getErrors()).to.be.an('array');
+    expect(result.getErrors()).to.have.lengthOf(0);
+  });
+
+  it('should return an error', () => {
+    const query = { where: [['secondName', '==', 'name']] };
+    const result = validateIndexedFields(indexedFields, query);
+
+    expect(result).to.be.an.instanceOf(ValidationResult);
+    expect(result.isValid()).to.be.false();
+    expect(result.getErrors()).to.be.an('array');
+    expect(result.getErrors()).to.have.lengthOf(1);
+    expect(result.getErrors()[0]).to.be.an.instanceOf(ValidationError);
+    expect(result.getErrors()[0].message).to.be.equal('Search fields can only contain one of these fields: $userId, firstName, lastName, $id');
+  });
+
+  it('should check empty query', () => {
+    const query = {};
+    const result = validateIndexedFields(indexedFields, query);
+
+    expect(result).to.be.an.instanceOf(ValidationResult);
+    expect(result.isValid()).to.be.true();
+    expect(result.getErrors()).to.be.an('array');
+    expect(result.getErrors()).to.have.lengthOf(0);
+  });
+
+  it('should check empty document indices', () => {
+    delete indexedFields.indices;
+    const query = { where: [['secondName', '==', 'name']] };
+
+    const result = validateIndexedFields(indexedFields, query);
+
+    expect(result).to.be.an.instanceOf(ValidationResult);
+    expect(result.isValid()).to.be.false();
+    expect(result.getErrors()).to.be.an('array');
+    expect(result.getErrors()).to.have.lengthOf(1);
+    expect(result.getErrors()[0]).to.be.an.instanceOf(ValidationError);
+    expect(result.getErrors()[0].message).to.be.equal('Search fields can only contain one of these fields: $userId, firstName, lastName, $id');
+  });
+});
