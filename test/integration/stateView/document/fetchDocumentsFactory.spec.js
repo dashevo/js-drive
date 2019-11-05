ccf33bb918ec99396e9f7e66a8c1c0f05621ea60
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2019-11-05 16:17:27 +0300

    Fix PR comments

--- test/integration/stateView/document/fetchDocumentsFactory.spec.js
+++ test/integration/stateView/document/fetchDocumentsFactory.spec.js
@@ -24,7 +24,7 @@ describe('fetchDocumentsFactory', () => {
   let fetchDocuments;
   let mongoClient;
   let svDocument;
-  let type;
+  let documentType;
   let contractId;
   let document;
   let svContractMongoDbRepository;
@@ -68,16 +68,16 @@ describe('fetchDocumentsFactory', () => {
     [svDocument] = getSVDocumentsFixture();
 
     document = svDocument.getDocument();
-    type = document.getType();
+    documentType = document.getType();
 
     await svContractMongoDbRepository.store(svContract);
   });
 
   it('should fetch Documents for specified contract ID and document type', async () => {
-    const svDocumentRepository = createSVDocumentMongoDbRepository(contractId, type);
+    const svDocumentRepository = createSVDocumentMongoDbRepository(contractId, documentType);
     await svDocumentRepository.store(svDocument);
 
-    const result = await fetchDocuments(contractId, type);
+    const result = await fetchDocuments(contractId, documentType);
 
     expect(result).to.be.an('array');
     expect(result).to.have.lengthOf(1);
@@ -90,15 +90,15 @@ describe('fetchDocumentsFactory', () => {
   });
 
   it('should fetch Documents for specified contract id, document type and name', async () => {
-    let result = await fetchDocuments(contractId, type);
+    let result = await fetchDocuments(contractId, documentType);
 
     expect(result).to.deep.equal([]);
 
-    const svDocumentRepository = createSVDocumentMongoDbRepository(contractId, type);
+    const svDocumentRepository = createSVDocumentMongoDbRepository(contractId, documentType);
     await svDocumentRepository.store(svDocument);
 
     const query = { where: [['name', '==', document.get('name')]] };
-    result = await fetchDocuments(contractId, type, query);
+    result = await fetchDocuments(contractId, documentType, query);
 
     expect(result).to.be.an('array');
     expect(result).to.have.lengthOf(1);
@@ -111,69 +111,78 @@ describe('fetchDocumentsFactory', () => {
   });
 
   it('should return empty array for specified contract ID, document type and name not exist', async () => {
-    const svDocumentRepository = createSVDocumentMongoDbRepository(contractId, type);
+    const svDocumentRepository = createSVDocumentMongoDbRepository(contractId, documentType);
     await svDocumentRepository.store(svDocument);
 
     const query = { where: [['name', '==', 'unknown']] };
 
-    const result = await fetchDocuments(contractId, type, query);
+    const result = await fetchDocuments(contractId, documentType, query);
 
     expect(result).to.deep.equal([]);
   });
 
   it('should throw InvalidQueryError if contract ID does not exist', async () => {
-    const svDocumentRepository = createSVDocumentMongoDbRepository(contractId, type);
+    const svDocumentRepository = createSVDocumentMongoDbRepository(contractId, documentType);
 
     await svDocumentRepository.store(svDocument);
 
     contractId = 'Unknown';
 
     try {
-      await fetchDocuments(contractId, type);
+      await fetchDocuments(contractId, documentType);
 
       expect.fail('should throw InvalidQueryError');
     } catch (e) {
       expect(e).to.be.instanceOf(InvalidQueryError);
       expect(e.getErrors()).to.be.an('array');
       expect(e.getErrors()).to.have.lengthOf(1);
-      expect(e.getErrors()[0].message).to.be.equal('Invalid contract ID: Unknown');
+
+      const [error] = e.getErrors();
+
+      expect(error.getContractId()).to.be.equal(contractId);
     }
   });
 
   it('should throw InvalidQueryError if type does not exist', async () => {
-    const svDocumentRepository = createSVDocumentMongoDbRepository(contractId, type);
+    const svDocumentRepository = createSVDocumentMongoDbRepository(contractId, documentType);
 
     await svDocumentRepository.store(svDocument);
 
-    type = 'Unknown';
+    documentType = 'Unknown';
 
     try {
-      await fetchDocuments(contractId, type);
+      await fetchDocuments(contractId, documentType);
 
       expect.fail('should throw InvalidQueryError');
     } catch (e) {
       expect(e).to.be.instanceOf(InvalidQueryError);
       expect(e.getErrors()).to.be.an('array');
       expect(e.getErrors()).to.have.lengthOf(1);
-      expect(e.getErrors()[0].message).to.be.equal('Invalid document type: Unknown');
+
+      const [error] = e.getErrors();
+
+      expect(error.getDocumentType()).to.be.equal(documentType);
     }
   });
 
   it('should throw InvalidQueryError if searching by non indexed fields', async () => {
-    const svDocumentRepository = createSVDocumentMongoDbRepository(contractId, type);
+    const svDocumentRepository = createSVDocumentMongoDbRepository(contractId, documentType);
     await svDocumentRepository.store(svDocument);
 
     const query = { where: [['lastName', '==', 'unknown']] };
 
     try {
-      await fetchDocuments(contractId, type, query);
+      await fetchDocuments(contractId, documentType, query);
 
       expect.fail('should throw InvalidQueryError');
     } catch (e) {
       expect(e).to.be.instanceOf(InvalidQueryError);
       expect(e.getErrors()).to.be.an('array');
       expect(e.getErrors()).to.have.lengthOf(1);
-      expect(e.getErrors()[0].message).to.be.equal('Query by not indexed field "lastName" is not allowed');
+
+      const [error] = e.getErrors();
+
+      expect(error.getNotIndexedField()).to.be.equal('lastName');
     }
   });
 });
