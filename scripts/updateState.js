d29d598ab6a40d2748a1b39a7d4c05bf5fd473e8
shuplenkov <konstantin.shuplenkov@dash.org>
2019-12-20 19:20:34 +0300

    Bump version

--- scripts/updateState.js
+++ scripts/updateState.js
@@ -1,14 +1,23 @@
 console.log('updateState before');
 require('dotenv-expand')(require('dotenv-safe').config());
 
+console.log('dotenv');
 const grpc = require('grpc');
 
+console.log('grpc');
 const { server: { createServer } } = require('@dashevo/grpc-common');
+
+console.log('createServer');
 const { getUpdateStateDefinition } = require('@dashevo/drive-grpc');
 
+console.log('getUpdateStateDefinition');
+
 const UpdateStateApp = require('../lib/app/UpdateStateApp');
+
+console.log('UpdateStateApp');
 const UpdateStateAppOptions = require('../lib/app/UpdateStateAppOptions');
 
+console.log('UpdateStateAppOptions');
 const errorHandler = require('../lib/util/errorHandler');
 
 console.log('Starting updateState');
