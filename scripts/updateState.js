ccb1504f6545ca10a0f58bf39993803e1cd45d83
shuplenkov <konstantin.shuplenkov@dash.org>
2019-12-20 15:32:48 +0300

    updateState logs

--- scripts/updateState.js
+++ scripts/updateState.js
@@ -13,18 +13,19 @@ const errorHandler = require('../lib/util/errorHandler');
 (async function main() {
   const updateStateAppOptions = new UpdateStateAppOptions(process.env);
   const updateStateApp = new UpdateStateApp(updateStateAppOptions);
-
+  console.log('Starting updateState');
   await updateStateApp.init();
-
+  console.log('updateState initialized');
   const grpcServer = createServer(
     getUpdateStateDefinition(),
     updateStateApp.createWrappedHandlers(),
   );
-
+  console.log('updateState', 'server created');
   grpcServer.bind(
     `${updateStateAppOptions.getGrpcHost()}:${updateStateAppOptions.getGrpcPort()}`,
     grpc.ServerCredentials.createInsecure(),
   );
-
+  console.log('updateState', `${updateStateAppOptions.getGrpcHost()}:${updateStateAppOptions.getGrpcPort()}`);
   grpcServer.start();
+  console.log('updateState started');
 }()).catch(errorHandler);
