8a6ff74fb026d090990ea8e617477b411cfecb73
Djavid Gabibiyan <djavidoff@gmail.com>
2020-05-19 15:42:07 +0500

    refactor: store only first public key

--- test/unit/dpp/DriveStateRepository.spec.js
+++ test/unit/dpp/DriveStateRepository.spec.js
@@ -115,17 +115,13 @@ describe('DriveStateRepository', () => {
       expect(blockExecutionDBTransactionsMock.getTransaction).to.be.calledOnceWith('identity');
       expect(identityRepositoryMock.store).to.be.calledOnceWith(identity, transactionMock);
 
-      expect(publicKeyIdentityIdRepositoryMock.store).to.have.callCount(
-        identity.getPublicKeys().length,
-      );
+      const [firstPublicKey] = identity.getPublicKeys();
 
-      identity.getPublicKeys().forEach((publicKey, index) => {
-        expect(publicKeyIdentityIdRepositoryMock.store.getCall(index).args).to.deep.equal([
-          publicKey.hash(),
-          identity.getId(),
-          transactionMock,
-        ]);
-      });
+      expect(publicKeyIdentityIdRepositoryMock.store).to.have.been.calledOnceWithExactly(
+        firstPublicKey.hash(),
+        identity.getId(),
+        transactionMock,
+      );
     });
   });
 
