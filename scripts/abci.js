cb1c037ce6b054749426e62d9e16aa310d76dc57
Djavid Gabibiyan <djavidoff@gmail.com>
2020-05-25 14:32:46 +0500

    refactor: use callback to report progress

--- scripts/abci.js
+++ scripts/abci.js
@@ -9,8 +9,15 @@ const errorHandler = require('../lib/errorHandler');
 (async function main() {
   const container = await createDIContainer(process.env);
 
+  const logger = container.resolve('logger');
+
+  logger.info('checking Core has finished syncing...');
   const checkCoreSyncFinished = container.resolve('checkCoreSyncFinished');
-  await checkCoreSyncFinished();
+  await checkCoreSyncFinished((currentBlockHeight, currentHeaderNumber) => {
+    logger.info(
+      `currently synced ${currentBlockHeight} blocks and ${currentHeaderNumber} headers`,
+    );
+  });
 
   const server = createServer(
     container.resolve('abciHandlers'),
