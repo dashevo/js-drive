256b1696ae7840ef04fd4b4468f0e1b9060d6c44
shuplenkov <konstantin.shuplenkov@dash.org>
2020-05-26 13:39:30 +0300

    Wait mongoDB replica set to be initialized on start

---
+++ test/integration/mongoDb/waitReplicaSetInitializeFactory.spec.js
@@ -0,0 +1,32 @@
+const { startMongoDb, startDashCore } = require('@dashevo/dp-services-ctl');
+
+const createTestDIContainer = require('../../../lib/test/createTestDIContainer');
+
+describe('waitReplicaSetInitializeFactory', function main() {
+  this.timeout(90000);
+
+  let mongoDB;
+  let dashCore;
+
+  before(async () => {
+    // mongoDB = await startMongoDb();
+    dashCore = await startDashCore();
+  });
+
+  after(async () => {
+    await mongoDB.remove();
+  });
+
+  it('should wait until mongodb replica set is initialed', async () => {
+    mongoDB = await startMongoDb();
+
+    await createTestDIContainer(mongoDB, dashCore);
+
+    const status = await mongoDB.getClient().db('test')
+      .admin()
+      .command({ replSetGetStatus: 1 });
+
+    const isInitialized = status && status.members && status.members[0] && status.members[0].stateStr === 'PRIMARY';
+    expect(isInitialized).to.be.true();
+  });
+});
