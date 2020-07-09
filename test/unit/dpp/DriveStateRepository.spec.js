b16fc63848ebd4420b42ecad0b9eb4a79b2529cb
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2020-07-09 20:37:51 +0300

    Add fetchLatestPlatformBlockHeader method to DriveStateRepository

--- test/unit/dpp/DriveStateRepository.spec.js
+++ test/unit/dpp/DriveStateRepository.spec.js
@@ -18,6 +18,7 @@ describe('DriveStateRepository', () => {
   let documents;
   let dataContract;
   let transactionMock;
+  let blockExecutionStateMock;
 
   beforeEach(function beforeEach() {
     identity = getIdentityFixture();
@@ -52,6 +53,10 @@ describe('DriveStateRepository', () => {
 
     createDocumentRepositoryMock = this.sinon.stub();
 
+    blockExecutionStateMock = {
+      getHeader: this.sinon.stub(),
+    };
+
     stateRepository = new DriveStateRepository(
       identityRepositoryMock,
       publicKeyIdentityIdRepositoryMock,
@@ -60,6 +65,7 @@ describe('DriveStateRepository', () => {
       createDocumentRepositoryMock,
       coreRpcClientMock,
       blockExecutionDBTransactionsMock,
+      blockExecutionStateMock,
     );
 
     transactionMock = {};
@@ -247,6 +253,24 @@ describe('DriveStateRepository', () => {
         expect(e).to.equal(error);
         expect(coreRpcClientMock.getRawTransaction).to.be.calledOnceWith(id);
       }
+    });
+  });
+
+  describe('#fetchLatestPlatformBlockHeader', () => {
+    it('should fetch latest platform block header', async () => {
+      const header = {
+        height: 10,
+        time: {
+          seconds: Math.ceil(new Date().getTime() / 1000),
+        },
+      };
+
+      blockExecutionStateMock.getHeader.resolves(header);
+
+      const result = await stateRepository.fetchLatestPlatformBlockHeader();
+
+      expect(result).to.deep.equal(header);
+      expect(blockExecutionStateMock.getHeader).to.be.calledOnce();
     });
   });
 });
