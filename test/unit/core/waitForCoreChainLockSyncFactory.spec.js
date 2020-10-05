8261d58697622b5f128d464bcc3fd567be155281
Alex Werner <alex@werner.fr>
2020-10-05 13:13:11 +0200

    tests: sanitize + waitForCoreChainLockSync

--- test/unit/core/waitForCoreChainLockSyncFactory.spec.js
+++ test/unit/core/waitForCoreChainLockSyncFactory.spec.js
@@ -1,24 +1,31 @@
+const EventEmitter = require('events');
+const ZMQClient = require('@dashevo/dashd-zmq');
 const LatestCoreChainLock = require('../../../lib/core/LatestCoreChainLock');
 const waitForCoreChainLockSyncFactory = require('../../../lib/core/waitForCoreChainLockSyncFactory');
 
 describe('waitForCoreChainLockSyncFactory', () => {
   let waitForCoreChainLockHandler;
-  let latestCoreChainLock;
   let coreRpcClientMock;
   let coreZMQClientMock;
+  const latestCoreChainLock = new LatestCoreChainLock();
 
   beforeEach(function beforeEach() {
-    latestCoreChainLock = new LatestCoreChainLock(this.sinon);
     coreRpcClientMock = {
-      getBestChainLock: this.sinon.stub(),
+      getBestChainLock: () => ({
+        result: {
+          height: 84202,
+          blockHash: '0000000007e0a65b763c0a4fb2274ff757abdbd19c9efe9de189f5828c70a5f4',
+          signature: '0a43f1c3e5b3e8dbd670bca8d437dc25572f72d8e1e9be673e9ebbb606570307c3e5f5d073f7beb209dd7e0b8f96c751060ab3a7fb69a71d5ccab697b8cfa5a91038a6fecf76b7a827d75d17f01496302942aa5e2c7f4a48246efc8d3941bf6c',
+        },
+        error: null,
+        id: 5
+      }),
       getBlock: this.sinon.stub(),
     };
-    coreZMQClientMock = {
-      connect: this.sinon.stub(),
-      subscribe: this.sinon.stub(),
-      on: this.sinon.stub(),
-      emit: this.sinon.stub(),
-    };
+    coreZMQClientMock = new EventEmitter();
+    coreZMQClientMock.connect = this.sinon.stub();
+    coreZMQClientMock.subscribe = this.sinon.stub();
+
     const loggerMock = {
       debug: this.sinon.stub(),
       info: this.sinon.stub(),
@@ -33,6 +40,35 @@ describe('waitForCoreChainLockSyncFactory', () => {
 
   it('should wait for chainlock to be synced', async () => {
     const response = await waitForCoreChainLockHandler();
-    expect(response.latestCoreChainLock).to.be.an.instanceOf(LatestCoreChainLock);
+    expect(response)
+      .to
+      .be
+      .an
+      .instanceOf(LatestCoreChainLock);
+  });
+  it('should handle when no chainlock is found via RPC', (done) => {
+    coreRpcClientMock.getBestChainLock = async () => {
+      const err = new Error();
+      err.code = -32603;
+      err.message = 'Chainlock not found';
+      throw err;
+    };
+    waitForCoreChainLockHandler()
+      .then((response) => {
+        expect(response)
+          .to
+          .be
+          .an
+          .instanceOf(LatestCoreChainLock);
+        done();
+      });
+    setTimeout(async () => {
+      coreZMQClientMock.emit(ZMQClient.TOPICS.rawchainlock, {
+        height: 84202,
+        blockHash: '0000000007e0a65b763c0a4fb2274ff757abdbd19c9efe9de189f5828c70a5f4',
+        signature: '0a43f1c3e5b3e8dbd670bca8d437dc25572f72d8e1e9be673e9ebbb606570307c3e5f5d073f7beb209dd7e0b8f96c751060ab3a7fb69a71d5ccab697b8cfa5a91038a6fecf76b7a827d75d17f01496302942aa5e2c7f4a48246efc8d3941bf6c',
+      });
+      coreZMQClientMock.emit(ZMQClient.TOPICS.hashblock, '0000000007e0a65b763c0a4fb2274ff757abdbd19c9efe9de189f5828c70a5f4');
+    }, 10);
   });
 });
