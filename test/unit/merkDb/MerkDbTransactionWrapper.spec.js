a74901158e5b78e87cc49681243d9d1479ca7f3e
Djavid Gabibiyan <djavidoff@gmail.com>
2020-10-30 20:39:02 +0500

    chore: some tests

--- test/unit/merkDb/MerkDbTransactionWrapper.spec.js
+++ test/unit/merkDb/MerkDbTransactionWrapper.spec.js
@@ -25,27 +25,66 @@ describe('MerkDbTransactionWrapper', () => {
 
   describe('#get', () => {
     it('should return value from transaction', () => {
+      const key = Buffer.from([1, 2, 3]);
+      const value = Buffer.from([4, 2]);
 
+      merkDbTransactionWrapper.data.set(key.toString('hex'), value);
+
+      const result = merkDbTransactionWrapper.get(key);
+
+      expect(result).to.deep.equal(value);
     });
 
     it('should return value from db', () => {
+      const key = Buffer.from([1, 2, 3]);
+      const value = Buffer.from([4, 2]);
 
+      merkDBMock.getSync.returns(value);
+
+      const result = merkDbTransactionWrapper.get(key);
+
+      expect(result).to.deep.equal(value);
     });
 
     it('should return null if value was removed in transaction', () => {
+      const key = Buffer.from([1, 2, 3]);
+      const value = null;
 
+      merkDbTransactionWrapper.deleted.add(key.toString('hex'));
+
+      const result = merkDbTransactionWrapper.get(key);
+
+      expect(result).to.deep.equal(value);
     });
   });
 
   describe('#put', () => {
     it('should put value into transaction', () => {
+      const key = Buffer.from([1, 2, 3]);
+      const value = Buffer.from([4, 2]);
 
+      merkDbTransactionWrapper.deleted.add(key.toString('hex'));
+
+      merkDbTransactionWrapper.put(key, value);
+
+      expect(merkDbTransactionWrapper.deleted.has(key.toString('hex'))).to.be.false();
+      expect(merkDbTransactionWrapper.data.get(key.toString('hex'))).to.deep.equals(
+        value,
+      );
     });
   });
 
   describe('#delete', () => {
     it('should delete key from transaction', () => {
+      const key = Buffer.from([1, 2, 3]);
+      const value = Buffer.from([4, 2]);
 
+      merkDbTransactionWrapper.data.set(key.toString('hex'), value);
+
+      merkDbTransactionWrapper.delete(key);
+
+      expect(merkDbTransactionWrapper.deleted.has(key.toString('hex'))).to.be.true();
+      expect(merkDbTransactionWrapper.data.get(key.toString('hex'))).to.be.undefined();
     });
   });
 
@@ -57,7 +96,16 @@ describe('MerkDbTransactionWrapper', () => {
 
   describe('#rollback', () => {
     it('should rollback transaction', () => {
+      const key = Buffer.from([1, 2, 3]);
+      const value = Buffer.from([4, 2]);
 
+      merkDbTransactionWrapper.deleted.add(key.toString('hex'));
+      merkDbTransactionWrapper.data.set(key.toString('hex'), value);
+
+      merkDbTransactionWrapper.rollback();
+
+      expect(merkDbTransactionWrapper.deleted.size).to.equal(0);
+      expect(merkDbTransactionWrapper.data.size).to.equal(0);
     });
   });
 });
