8339b35980e6dee05193aada6206df7704ecdff2
Ivan Shumkov <ivan@shumkov.ru>
2020-11-03 20:34:54 +0100

    feat: more fixes and tests

--- test/integration/merkDb/MerkDbStore.spec.js
+++ test/integration/merkDb/MerkDbStore.spec.js
@@ -104,69 +104,79 @@ describe('MerkDbStore', () => {
   });
 
   describe('#delete', () => {
-    it('should delete value', () => {
+    beforeEach(() => {
       merkDb.batch()
         .put(key, value)
+        // MerkDB doesn't delete the last key for some reason
+        // So we need to add an extra one to test delete functionality
+        // on empty database
+        .put(Buffer.alloc(1), Buffer.alloc(1))
         .commitSync();
+    });
 
-      // store.delete(key);
-
-      merkDb.batch()
-        .delete(key)
-        .commitSync();
+    it('should delete value', () => {
+      store.delete(key);
 
       try {
         merkDb.getSync(key);
 
         expect.fail('should throw key not found error');
       } catch (e) {
-        expect(e);
+        expect(e.message).to.startsWith('key not found');
       }
     });
 
-    it('should store value in transaction', async () => {
+    it('should delete value in transaction', async () => {
       const transaction = store.createTransaction();
 
       expect(transaction).to.be.instanceOf(MerkDbTransaction);
 
       transaction.start();
 
-      // store data in transaction
-      store.put(key, value, transaction);
+      // Delete a value from transaction
+      store.delete(key, transaction);
 
-      // check we don't have data in db before commit
-      try {
-        merkDb.getSync(key);
-
-        expect.fail('Should fail with NotFoundError error');
-      } catch (e) {
-        expect(e.message.startsWith('key not found')).to.be.true();
-      }
-
-      // check we can't fetch data without transaction
-      const notFoundValue = store.get(key);
-
-      expect(notFoundValue).to.be.null();
-
-      // check we can fetch data inside transaction
+      // Now it should be absent there
       const valueFromTransaction = store.get(key, transaction);
+      expect(valueFromTransaction).to.be.null();
 
-      expect(valueFromTransaction).to.deep.equal(value);
+      // But should be still present in store
+      const valueFromStore = store.get(key);
+      expect(valueFromStore).to.deep.equal(value);
 
       await transaction.commit();
 
-      // check we have data in db after commit
-      const storedValue = merkDb.getSync(key);
-
-      expect(storedValue).to.deep.equal(value);
+      // When we commit transaction this key should disappear from store too
+      const valueFromStoreAfterCommit = store.get(key);
+      expect(valueFromStoreAfterCommit).to.be.null();
     });
   });
 
   describe('#getRootHash', () => {
+    it('should return a empty hash for empty store', () => {
+      const result = store.getRootHash();
 
+      expect(result).to.deep.equal(Buffer.alloc(20));
+    });
+
+    it('should return a root hash for store with value', () => {
+      merkDb.batch()
+        .put(key, value)
+        .commitSync();
+
+      const valueHash = Buffer.from('8431f62d3d7b7f16ec98321019d0e65ad191bea1', 'hex');
+
+      const result = store.getRootHash();
+
+      expect(result).to.deep.equal(valueHash);
+    });
   });
 
   describe('#createTransaction', () => {
+    it('should create a transaction', () => {
+      const result = store.createTransaction();
 
+      expect(result).to.be.instanceOf(MerkDbTransaction);
+    });
   });
 });
