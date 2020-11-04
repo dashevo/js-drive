f50afd588e57a80ecfc74777fa2aedeef74beca1
Cofresi <cofresi@dash.org>
2020-11-04 11:45:27 -0400

    fix tests

--- test/unit/abci/handlers/query/verifyChainlockQueryHandlerFactory.spec.js
+++ test/unit/abci/handlers/query/verifyChainlockQueryHandlerFactory.spec.js
@@ -5,10 +5,7 @@ const {
 } = require('abci/types');
 
 const { ChainLock } = require('@dashevo/dashcore-lib');
-const SimplifiedMasternodeListFixtures = require('@dashevo/dashcore-lib/test/fixtures/mnList');
-const SimplifiedMasternodeList = require('../../../../../lib/core/SimplifiedMasternodeList');
 const verifyChainlockQueryHandlerFactory = require('../../../../../lib/abci/handlers/query/verifyChainlockQueryHandlerFactory');
-
 const InvalidArgumentAbciError = require('../../../../../lib/abci/errors/InvalidArgumentAbciError');
 const AbciError = require('../../../../../lib/abci/errors/AbciError');
 
@@ -24,52 +21,56 @@ const chainlockJSONinvalid = {
   signature: '071476c699fee312a29c0e7a604a5288237073e9317ac458f5772e0e40793fcca83ba72fe3b8f42f4cf1499c02764fb313b6661e873b084bb8e65cd087567060743fca85a73782a6f53503d4c336cc07b69780c6b9e98a4bfcce0d4b17d3d889',
 };
 
-describe('identityQueryHandlerFactory', () => {
-  let smlDiffs;
-  let simplifiedMasternodeList;
+describe('verifyChainlockQueryHandlerFactory', () => {
+  let simplifiedMasternodeListMock;
+  let decodeChainLockMockValid;
+  let decodeChainLockMockInvalid;
   let verifyChainlockQueryHandler;
   let chainlock;
   let chainlockInvalid;
   let params;
-  let data;
-  let dataInvalid;
+  let chainlockDataValid;
+  let chainlockDataInvalid;
 
-  beforeEach(() => {
-    smlDiffs = SimplifiedMasternodeListFixtures.getChainlockDiffArray();
-    simplifiedMasternodeList = new SimplifiedMasternodeList();
-    simplifiedMasternodeList.applyDiffs(smlDiffs);
-    verifyChainlockQueryHandler = verifyChainlockQueryHandlerFactory(
-      simplifiedMasternodeList,
-    );
-
+  beforeEach(function beforeEach() {
     chainlock = new ChainLock(chainlockJSON);
     chainlockInvalid = new ChainLock(chainlockJSONinvalid);
     params = {};
-    data = {
+    chainlockDataValid = {
       chainLock: chainlock.toBuffer(),
+      verify: this.sinon.stub().returns(true),
     };
-    dataInvalid = {
+    chainlockDataInvalid = {
       chainLock: chainlockInvalid.toBuffer(),
+      verify: this.sinon.stub().returns(false),
+    };
+    decodeChainLockMockValid = this.sinon.stub().returns(chainlockDataValid);
+    decodeChainLockMockInvalid = this.sinon.stub().returns(chainlockDataInvalid);
+    simplifiedMasternodeListMock = {
+      getStore: this.sinon.stub(),
     };
   });
 
   it('should validate a valid chainlock', async () => {
-    const result = await verifyChainlockQueryHandler(params, data);
+    verifyChainlockQueryHandler = verifyChainlockQueryHandlerFactory(
+      simplifiedMasternodeListMock, decodeChainLockMockValid,
+    );
+    const result = await verifyChainlockQueryHandler(params, chainlockDataValid);
 
     expect(result).to.be.an.instanceof(ResponseQuery);
     expect(result.code).to.equal(0);
   });
 
   it('should throw InvalidArgumentAbciError if chainlock is not valid', async () => {
+    verifyChainlockQueryHandler = verifyChainlockQueryHandlerFactory(
+      simplifiedMasternodeListMock, decodeChainLockMockInvalid,
+    );
     try {
-      await verifyChainlockQueryHandler(params, dataInvalid);
-
+      await verifyChainlockQueryHandler(params, chainlockDataInvalid);
       expect.fail('should throw InvalidArgumentAbciError');
     } catch (e) {
       expect(e).to.be.an.instanceof(InvalidArgumentAbciError);
       expect(e.getCode()).to.equal(AbciError.CODES.INVALID_ARGUMENT);
-      expect(e.message).to.equal(`Signature invalid for chainlock
-         ${chainlock}`);
     }
   });
 });
