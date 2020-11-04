3e941ebc460b2aa5a74d6a0caf75d8f02112f454
Cofresi <cofresi@dash.org>
2020-11-04 12:07:22 -0400

    test error message

--- test/unit/abci/handlers/query/verifyChainlockQueryHandlerFactory.spec.js
+++ test/unit/abci/handlers/query/verifyChainlockQueryHandlerFactory.spec.js
@@ -71,6 +71,7 @@ describe('verifyChainlockQueryHandlerFactory', () => {
     } catch (e) {
       expect(e).to.be.an.instanceof(InvalidArgumentAbciError);
       expect(e.getCode()).to.equal(AbciError.CODES.INVALID_ARGUMENT);
+      expect(e.message).to.equal('Signature invalid for chainlock');
     }
   });
 });
