02009e957edb105346b8bb7bd2a4492ac105ea6d
Djavid Gabibiyan <djavidoff@gmail.com>
2021-07-29 18:11:42 +0500

    fix: invalid flag array was constructed

--- test/unit/rootTree/convertRootTreeProofToBuffer.spec.js
+++ test/unit/rootTree/convertRootTreeProofToBuffer.spec.js
@@ -6,12 +6,16 @@ describe('convertRootTreeProofToBuffer', () => {
   beforeEach(() => {
     proof = [
       {
-        position: 'left',
-        data: Buffer.from('dcd980a26eb0b96668c972357fc67bf02169f884', 'hex'),
+        position: 'right',
+        data: Buffer.from('388f19cf5b6f2e1331e2c9130792972070d0ae38', 'hex'),
       },
       {
         position: 'right',
         data: Buffer.from('388f19cf5b6f2e1331e2c9130792972070d0ae37', 'hex'),
+      },
+      {
+        position: 'left',
+        data: Buffer.from('dcd980a26eb0b96668c972357fc67bf02169f884', 'hex'),
       },
     ];
   });
@@ -21,7 +25,7 @@ describe('convertRootTreeProofToBuffer', () => {
 
     expect(result).to.be.an.instanceOf(Buffer);
     expect(result.toString('hex')).to.equal(
-      '0100000002dcd980a26eb0b96668c972357fc67bf02169f884388f19cf5b6f2e1331e2c9130792972070d0ae370101',
+      '0100000003388f19cf5b6f2e1331e2c9130792972070d0ae38388f19cf5b6f2e1331e2c9130792972070d0ae37dcd980a26eb0b96668c972357fc67bf02169f8840104',
     );
   });
 });
