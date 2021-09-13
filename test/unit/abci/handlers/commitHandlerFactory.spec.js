ef80e52a53229880823d167748454798c969e8a1
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2021-09-13 20:16:52 +0300

    update test suite branch with a test that tests the issue

--- test/unit/abci/handlers/commitHandlerFactory.spec.js
+++ test/unit/abci/handlers/commitHandlerFactory.spec.js
@@ -160,7 +160,7 @@ describe('commitHandlerFactory', () => {
       loggerMock,
       cloneToPreviousStoreTransactionsMock,
       getLatestFeatureFlagMock,
-      previousRootTreeMock
+      previousRootTreeMock,
     );
   });
 
